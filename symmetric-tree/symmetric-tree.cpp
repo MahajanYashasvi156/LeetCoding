a6d7ae629708d5a0122ed9366240d191031028ad
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2021-12-24 20:08:15 +0530

    Time: 8 ms (32.79%), Space: 16.3 MB (92.02%) - LeetHub

--- symmetric-tree/symmetric-tree.cpp
+++ symmetric-tree/symmetric-tree.cpp
@@ -9,23 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/*
+Similar to identical tree logic, just a difference is we will check left subtree with right subtree and vice versa.
+
+Recursive -
+
+TC      - O(N)
+Aux SC  - O(N)
+SC      - O(1)
+*/
 class Solution {
 public:
     bool check(TreeNode* leftNode, TreeNode* rightNode)
     {
-        if(!leftNode and !rightNode)
+        if(leftNode==NULL and rightNode==NULL)
             return true;
         
         if(leftNode and rightNode and leftNode->val == rightNode->val)
             return check(leftNode->left,rightNode->right) and check(leftNode->right,rightNode->left);
         
+        //When node's values are diffrent or one of the node is NULL.
         return false; 
     }
     bool isSymmetric(TreeNode* root) 
     {
-        if(root==NULL or (!root->left and !root->right))
-            return true;
         return check(root->left,root->right);
-        
     }
 };
