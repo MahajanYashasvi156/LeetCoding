5d00eb1f274c915789612695ffdbb40e7c02afe5
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2021-12-24 19:30:10 +0530

    Time: 12 ms (90.13%), Space: 26.5 MB (20.03%) - LeetHub

---
+++ construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+public:
+    TreeNode* buildTreeUtility(vector<int>& postorder,vector<int>& inorder,unordered_map<int,int> &indexInorder,int low,int high,int &postorderIndex)
+        //postorderIndex is call by refrence as after left call preorderIndex should be updated depending upon the left subtree nodes and now it tells the index of the nodes that are in the rightsubtree.
+    {
+        //Invalid Case when there will be no node in the range of inorder traversal
+        if(high<low)
+            return NULL;
+        
+        TreeNode* newNode=new TreeNode(postorder[postorderIndex]);
+        
+        int rootIndex = indexInorder[postorder[postorderIndex]] ;
+        postorderIndex--;
+        
+        newNode ->right = buildTreeUtility( postorder,inorder,indexInorder,rootIndex+1,high,postorderIndex);
+        newNode ->left  = buildTreeUtility( postorder,inorder,indexInorder,low,rootIndex-1,postorderIndex);
+       
+        
+        return newNode;
+    }
+    
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) 
+    {
+        unordered_map<int,int> indexInorder;
+        int postorderIndex=postorder.size()-1;
+        
+        for(int i=0;i<inorder.size();i++)
+            indexInorder[inorder[i]]=i;
+     
+        return buildTreeUtility( postorder,inorder,indexInorder,0,inorder.size()-1,postorderIndex);
+    }
+};
