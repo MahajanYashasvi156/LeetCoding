cb4ebc972ae1b6fd2b6345f20287e9ab1ddbda10
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2021-12-25 18:08:30 +0530

    Time: 28 ms (80.05%), Space: 23.3 MB (11.25%) - LeetHub

--- lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,30 +1,49 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/submissions/
 
-class Solution {
-public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL)
-            return NULL;
-        if(root==p)
-            return p;
-        if(root==q)
-            return q;
+    /*
+        Intuition - 
+        If both nodes are on left subtree then go to left subtree
+        else If both nodes are on right subtree then go to right subtree
+        else If both nodes are on different side of root then root is our LCA
+        (If both nodes are on diffrent side then current node will be the ancestor node. )
+    
+    Approach 1 Recursive-
+
+    TC - O(N) SC O(1) Aux Space O(N)
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
         
-        TreeNode* leftCheck = lowestCommonAncestor(root->left,p,q);
-        TreeNode* rightCheck = lowestCommonAncestor(root->right,p,q);
+        if(p->val <root->val and q->val<root->val) 
+            return lowestCommonAncestor(root->left,p,q);
+        if(p->val >root->val and q->val>root->val) 
+            return lowestCommonAncestor(root->right,p,q);
+        return root;
+            
         
-        if( leftCheck and rightCheck)
-            return root;
-        if(leftCheck)
-            return leftCheck;
-        return rightCheck;
     }
+    Approach 2- Recursive 
+    TC - O(N) SC O(1) Aux Space O(N)
+    */
+
+    class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        TreeNode* curr=root;
+        while(curr)
+        {
+            if(p->val <curr->val and q->val<curr->val) 
+                curr=curr->left;
+            else if(p->val >curr->val and q->val>curr->val)
+                curr=curr->right;
+            else
+                return curr;
+        }
+        return curr;
+    }
+    
+    
 };
+
+
