08e7a498d0315c1bc2ca66a7add07a3e929dcba9
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2021-12-26 14:03:09 +0530

    Time: 40 ms (58.35%), Space: 38.9 MB (34.04%) - LeetHub

--- two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -9,6 +9,16 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/*
+Link - https://leetcode.com/problems/two-sum-iv-input-is-a-bst/submissions/
+
+TC- O(N)
+SC- O(N)
+
+Intution - Store the traversed(any traversal here we have done inorder traversal) nodes values in the map. If the target number - current value is already present in the map that means pair of two elements whose sum = target number exist in the tree.
+
+
 class Solution 
 {
 public:
@@ -27,13 +37,38 @@ public:
             }
             curr=s.top();
             s.pop();
-            rem=k-curr->val;
-            if(mp.find(rem)!=mp.end())
+            if(mp.find(k-curr->val)!=mp.end())
                 return true;
-            mp[curr->val]++;
+            mp[curr->val]=1;
             curr=curr->right;
         }
-        
         return false;
+    }
+};
+
+*/
+/*
+Recursive 
+
+TC- O(N)
+ASC- O(N)
+SC- O(N)
+*/
+class Solution
+{
+public:
+    map<int,int> mp;
+    bool findTarget(TreeNode* root, int k) 
+    {
+        if(root==NULL)
+            return false;
+        bool left=findTarget(root->left,k);
+        if(left==true)
+            return  true;
+        if(mp.find(k-root->val)!=mp.end())
+            return true;
+        mp[root->val]=1;
+        return findTarget(root->right,k);
+        
     }
 };
