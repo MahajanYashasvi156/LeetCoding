17b386e8a785b1b8fd77245c69d9b5454a7c2bee
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2021-12-30 18:10:34 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (85.78%) - LeetHub

--- unique-binary-search-trees/unique-binary-search-trees.cpp
+++ unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,28 +1,25 @@
-class Solution {
+class Solution
+{
 public:
-long double factorial(int n)
-{
-long double fact=1;
-for(int i=n;i>n/2;i--)
-{
-fact*=i;
-}
-return fact;
-}
-long double factorial1(int n)
-{
-long double fact=1;
-for(int i=2;i<=n;i++)
-{
-fact*=i;
-}
-return fact;
-}
-int numTrees(int n) {
-int ans;
-long double num;
-num=factorial(2*n);
-ans=num/factorial1(n+1);
-return ans;
-}
+    long double factnum(int n)
+    {
+        long double f=1;
+        for(int i=n;i>n/2;i--)
+            f=f*i;
+        return f;
+    }
+    long double factden(int n)
+    {
+        long double f=1;
+        for(int i=2;i<=n;i++)
+            f=f*i;
+        return f;
+    }
+    
+    int numTrees(int n) 
+    {
+        long double num=factnum(2*n);
+        long double den=factden(n+1);
+        return num/den;
+    }
 };
