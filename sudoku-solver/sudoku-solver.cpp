29b3a7c8818dea0ffe95b47b1894f2d2760192a5
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-01-03 09:02:47 +0530

    Time: 20 ms (73.15%), Space: 6.4 MB (56.59%) - LeetHub

--- sudoku-solver/sudoku-solver.cpp
+++ sudoku-solver/sudoku-solver.cpp
@@ -1,57 +1,69 @@
+/*
+Link - https://leetcode.com/problems/sudoku-solver/submissions/
+Approach - We have to fill entire board, so go recursively on each cell in row major form while trying all 9 options in the cell.
+Before trying any option in empty cell check if trying that option will make sudoku invalid
+If sudoku is valid, try that option and go to next cell
+If sudoku is invalid, then try different option
+
+If cell is already filled, then just move to next cell
+TC - O(9^(n^2))
+ASC- O(n^2)
+SC - O(1)
+*/
 class Solution 
 {
 public:
-    bool isValidSudoku(vector<vector<char>>& board)
+     bool isValidSudoku(vector<vector<char>>& board,int row,int col,char c)
     {
-        int checker[10];
-        
-        //Check Validity of Row
+         //Check for row
         for(int i=0;i<9;i++)
         {
-            memset(checker,0,sizeof(checker));
-            for(int j=0;j<9;j++)
-            {
-                if(board[i][j]!='.' and checker[board[i][j]-48]!=0)
-                    return false;
-                else if(board[i][j]!='.')
-                    checker[board[i][j]-48]=1;
-            } 
+            if(board[i][col] == c)
+                return false;
         }
-        
-        //Check Validity of Column
-        for(int i=0;i<9;i++)
+         
+         //Check for column
+         for(int i=0;i<9;i++)
+         {
+            if(board[row][i] == c)
+                return false;
+         }
+         
+        //Check for block - row/3 and col/3 to go to the beginning of the block.
+        row=row/3;
+        col=col/3;
+         
+        for(int i=3*row;i<3*row+3;i++)
         {
-            memset(checker,0,sizeof(checker));
-            for(int j=0;j<9;j++)
+            for(int j=3*col;j<3*col+3;j++)
             {
-                if(board[j][i]!='.' and checker[board[j][i]-48]!=0)
-                    return false;
-                else if(board[j][i]!='.')
-                    checker[board[j][i]-48]=1;
-            } 
-        }
-
-        //Check Validity of Block
-        for(int row=0;row<3;row++)
-        {
-            for(int col=0;col<3;col++)
-            {
-             memset(checker,0,sizeof(checker));
-                for(int i=row*3;i<row*3+3;i++)
-                {
-                    for(int j=col*3;j<col*3+3;j++)
-                    {
-                        if(board[i][j]!='.' and checker[board[i][j]-48]!=0)
-                            return false;
-                        else if(board[i][j]!='.')
-                            checker[board[i][j]-48]=1;
-                    } 
-                }
+                if(board[i][j]==c)
+                     return false;
             }
         }
-    
         return true;
     }
+    
+    /*
+    bool isValidSudoku(vector<vector<char>>& board,int row,int col,char c)
+    {
+        for(int i=0;i<9;i++)
+        {
+            //Check for row
+            if(board[i][col] == c)
+                return false;
+            
+            //Check for column
+            if(board[row][i] == c)
+                return false;
+            
+            //Check for block - row/3 and col/3 to go to the beginning of the block.
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
+                return false;
+        }
+        return true;
+    }
+    */
     bool solveSudokuUtility(vector<vector<char>>& board,int c)
     {
         int row=c/9;
@@ -63,26 +75,20 @@ public:
         {
            for(char i = '1';i<='9';i++)
            {
-               board[row][col]= i;
-               if(isValidSudoku(board))
+               if(isValidSudoku(board,row,col,i))
                {
-                   if(solveSudokuUtility(board,c+1))
-                        return true;
-                 
-               }
-               board[row][col]= '.';
+
+                  board[row][col]= i;
+                  if(solveSudokuUtility(board,c+1))
+                    return true;
+                  board[row][col]= '.';
+               } 
            }
         }
-        else
-        {
-            if(solveSudokuUtility(board,c+1))
-                   return true;
-                        
-        }
+        else if(solveSudokuUtility(board,c+1))
+                return true;
         return false;
-        
     }
-    
     void solveSudoku(vector<vector<char>>& board) 
     {
         solveSudokuUtility(board,0);
