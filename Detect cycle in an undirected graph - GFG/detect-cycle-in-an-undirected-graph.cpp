7e1a63bd96746470b1a37e9856016cc30212c0c2
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-01-05 19:30:13 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -6,59 +6,33 @@ using namespace std;
 class Solution 
 {
   public:
-    /*
-    Link - https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1#
     
-    TC - O(V+E)
-    SC - O(V)
-    
-    Approach - Apply BFS to detect cycle in an undirected graph.
-    
-    As we know, in BFS we use visited array to not visit a node again and to avoid infinite loop
-    
-    So in our approach, for an undirected graph we have to maintain parent of the node.
-    
-    In BFS if the visited node again appears as the ajacent node during traversal and this node is not the parent node then there is a cycle.
-    */
-    bool checkCycle(int src,vector<int> adj[],vector<bool> &visited)
+    bool checkCycle(int source,int parent,vector<int> adj[],vector<bool> &visited)
     {
-        queue<pair<int,int>> q; //queue(child,par)
-        
-        visited[src]=1;
-        q.push({src,-1});
-        
-        while(!q.empty())
+        visited[source]=true;
+        for(int destination:adj[source])
         {
-            int start=q.front().first;
-            int parent=q.front().second;
-            q.pop();
-            
-            for(int end:adj[start])
+            if(visited[destination]==false)
             {
-                if(visited[end]==false)
-                {
-                    visited[end]=true;
-                    q.push({end,start});
-                }
-                //end node already visited and it is not start's parent, so cycle detected
-                else if(end!=parent)
+                if(checkCycle(destination,source,adj,visited))
                     return true;
             }
+            else if (destination!=parent)
+                return true;
         }
-        //Cycle not detected.
         return false;
     }
+    
     // Function to detect cycle in an undirected graph.
     bool isCycle(int V, vector<int> adj[]) 
     {
         vector<bool> visited(V);
-        //Since given graph can be disconnected. Call for all unviisted node so far considering it as src.
-        for(int src=0;src<V;src++)
+        
+        for(int source=0;source<V;source++)
         {
-            if(visited[src]==false)
+            if(visited[source]==false)
             {
-                //It will execute for single component and set visited for all nodes in that component.
-                if(checkCycle(src,adj,visited))
+                if(checkCycle(source,-1,adj,visited))
                     return true;
             }
         }
