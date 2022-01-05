cb9e6f1db9078dd6cedaf52ac270dd65d1b7685d
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-01-05 20:07:51 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -33,7 +33,6 @@ class Solution
                 }
             }
         }
-        
         return false;
     }
     
@@ -55,18 +54,23 @@ class Solution
 };
 
 /*
+/*
 Link - https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1#
 
+Same for all three Approach.
 TC - O(V+E)
 SC - O(V)
 
-Approach - Apply BFS to detect cycle in an undirected graph.
+Approach - 1 BFS Using Iteration
+
+Apply BFS to detect cycle in an undirected graph.
 
 As we know, in BFS we use visited array to not visit a node again and to avoid infinite loop
 
 So in our approach, for an undirected graph we have to maintain parent of the node.
 
 In BFS if the visited node again appears as the ajacent node during traversal and this node is not the parent node then there is a cycle.
+
 
 
 class Solution 
@@ -120,7 +124,13 @@ class Solution
     }
 };
 
-Appraoch - DFS using recursive
+/*
+Approach - 2  Using DFS Recursion
+
+TC - O(V+E)
+SC - O(V)
+Aux SC - O(V)
+
 class Solution 
 {
   public:
@@ -157,6 +167,64 @@ class Solution
         return false;
     }
 };
+
+/*
+Approach - 3 Using DFS Iteration
+
+TC - O(V+E)
+SC - O(V)
+
+
+class Solution 
+{
+  public:
+    
+    bool checkCycleUsingDFSIterative(int source,vector<int> adj[],vector<bool> &visited)
+    {
+        stack<pair<int,int>> s;//child,parent.
+        s.push({source,-1});
+        
+        while(!s.empty())
+        {
+            int start  = s.top().first;
+            int parent = s.top().second;
+            s.pop();
+            
+            //Already not explored then only explore that vertex.
+            if(visited[start]==false)
+            {
+                visited[start]=true;
+                for(int end : adj[start])
+                {
+                    if(visited[end]==false)
+                    {
+                        s.push({end,start});
+                    }
+                    else if(end!=parent)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) 
+    {
+        vector<bool> visited(V);
+        
+        for(int source=0;source<V;source++)
+        {
+            if(visited[source]==false)
+            {
+                if(checkCycleUsingDFSIterative(source,adj,visited))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
+
 
 */
 // { Driver Code Starts.
