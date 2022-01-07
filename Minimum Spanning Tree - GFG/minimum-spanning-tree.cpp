1cc17fbadc94292258a8ac1a713541d0c06cfb19
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-01-07 09:48:33 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -5,14 +5,141 @@ using namespace std;
  // } Driver Code Ends
 
 /*
-Path Compression and union by rank.
+Link - https://practice.geeksforgeeks.org/problems/minimum-spanning-tree/1#
 
-TC - O(ElogE + V) ~ O(ElogV)
-Detailed - ElogE for sorting all edges and  for each edge E we have to find use unionSet which takes O(4*alpha) time i.e. constant,in total V time.
+Kruskal Algorithm - 
 
-SC - O(3E +2V)
+1. sort the edge list using weight of edge.
+2. one by one pick edge from the sorted list add it to the MST if adding that edge wont create cycle.
 
+How to check if they can form cycle? 
 
+Appraoch - Use Disjoint Set Data structure for ensuring that cycle not created.
+
+If the source and destination nodes of that edge has same absolute parent(belong to same component),
+then adding that edge will create cycle.
+This is because, if the two nodes have same absolute parent that means they have path in between them i.e. they are connected
+
+Disjoint set data structure will maitain the disjoint sets i.e. connected components formed till now
+
+Approach 1 - Union by Find
+Here, when adding an edge dont form cycle, we will simply connect there absolute parents and make them
+a single set. Using this will keep the disjoint set tree skewed
+
+i.  Without path compression
+    Find can take O(V) time as skewed tree can be fored
+    TC
+    Find - O(V)
+    Union - O(V)
+    SC 
+    O(V)
+
+ii. With path compression
+    TC
+    Find - O(log V)
+    Union - O(log V)
+    SC 
+    O(V)
+
+Approach 2 - Union by Rank
+Here, when adding an edge dont form cycle, we will use rank(sometimes height) to connect there absolute parents and make them
+a single set. Using rank will keep the disjoint set tree balanced which help to improve find time complexiy
+
+While adding two sets, add smaller rank set to bigger rank set. 
+If same rank, then add anyhow and increase rank of abs parent of merged set
+
+i.  Without path compression
+    Find can take O(log V) time as balanced tree is formed
+    TC
+    Find - O(log V)
+    Union - O(log V)
+    SC - O(V)
+
+ii. With path compression
+    TC
+    Find - O(4 * alpha) = O(1)
+    Union - O(4 * alpha) = O(1)
+    SC - O(V)
+
+*/
+
+/*
+Approach 1) i) -  without path compression and union by rank.
+
+TC - O(ElogE+V^2) 
+Detailed - ElogE for sorting all edges and  for each edge E(precisely V-1) we have to find use unionSet which takes V times,in total V^2 time.
+
+SC - O(3E +V) = O(E+V)
+*/
+class Solution
+{
+    int findPar(vector<int> &parent,int X)
+    {
+          if(parent[X]==-1)
+                return X;
+          return findPar(parent,parent[X]);
+    }
+    
+    bool unionSet(vector<int> &parent,int X,int Z)
+    {
+    	int AbsoluteParentX = findPar(parent,X);
+    	int AbsoluteParentZ = findPar(parent,Z);
+    	
+    	if(AbsoluteParentX!=AbsoluteParentZ)
+    	{
+    	   parent[AbsoluteParentX]=AbsoluteParentZ;
+    	   return true;
+    	} 
+    	return false;
+    }
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+       vector<int>parent(V,-1);
+       
+       vector<pair<int,pair<int,int>>> edgeList;
+       int minSTCost = 0;
+       
+       for(int start=0; start<V;start++)
+       {
+           for(auto endList : adj[start])
+           {
+               int end = endList[0];
+               int weight = endList[1];
+               edgeList.push_back({weight,{start,end}});
+           }
+       }
+       
+       sort(edgeList.begin(),edgeList.end());
+       
+       int edgeCount = V-1;
+       for(auto a : edgeList)
+       {
+           int weight = a.first;
+           int start = a.second.first;
+           int end  = a.second.second;
+           if(unionSet(parent,start,end))
+           {
+                minSTCost += weight;
+                edgeCount--;
+                if(edgeCount==0)
+                    return minSTCost;
+           }
+       }
+        
+        return minSTCost;
+    }
+    
+};
+
+/*
+Approach 2) ii) - Union by Rank with path compression
+
+TC - O(ElogE + V) = O(ElogV)
+Detailed - ElogE for sorting all edges and  for each edge E(precisely V-1) we have to find use unionSet which takes O(4*alpha) time i.e. constant,so total V time.
+
+SC - O(3E +2V) = O(E+V)
 
 class Solution
 {
@@ -84,80 +211,10 @@ class Solution
     }
     
 };
+
+
+
 */
-/*
-Link - https://practice.geeksforgeeks.org/problems/minimum-spanning-tree/1#
-
-Without Path Compression and union by rank. 
-TC - O(E(LogE+V))
-Detailed - ElogE for sorting all edges and  for each edge E we have to find use unionSet which takes V times,in total EV time.
-
-SC - O(3E +V)
-*/
-
-class Solution
-{
-    int findPar(vector<int> &parent,int X)
-    {
-          if(parent[X]==-1)
-                return X;
-          return findPar(parent,parent[X]);
-    }
-    
-    bool unionSet(vector<int> &parent,int X,int Z)
-    {
-    	int AbsoluteParentX = findPar(parent,X);
-    	int AbsoluteParentZ = findPar(parent,Z);
-    	
-    	if(AbsoluteParentX!=AbsoluteParentZ)
-    	{
-    	   parent[AbsoluteParentX]=AbsoluteParentZ;
-    	   return true;
-    	} 
-    	return false;
-    }
-	public:
-	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[])
-    {
-       vector<int>parent(V,-1);
-       
-       vector<pair<int,pair<int,int>>> edgeList;
-       int minSTCost = 0;
-       
-       for(int start=0; start<V;start++)
-       {
-           for(auto endList : adj[start])
-           {
-               int end = endList[0];
-               int weight = endList[1];
-               edgeList.push_back({weight,{start,end}});
-           }
-       }
-       
-       sort(edgeList.begin(),edgeList.end());
-       
-       int edgeCount = V-1;
-       for(auto a : edgeList)
-       {
-           int weight = a.first;
-           int start = a.second.first;
-           int end  = a.second.second;
-           if(unionSet(parent,start,end))
-           {
-                minSTCost += weight;
-                edgeCount--;
-                if(edgeCount==0)
-                    return minSTCost;
-           }
-       }
-        
-        return minSTCost;
-    }
-    
-};
-
-
 
 /*
 Link - https://practice.geeksforgeeks.org/problems/minimum-spanning-tree/1
