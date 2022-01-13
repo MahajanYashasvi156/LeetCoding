fd6406739f7468073798936019e13ab5472a4298
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-01-13 18:06:05 +0530

    Time: 8 ms (91.35%), Space: 7.2 MB (61.78%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,8 +1,9 @@
 /*
 Link - https://leetcode.com/problems/coin-change-2/submissions/
 
-TC - 
-SC - 
+TC - O(2^coins)
+ASC - O(coins)
+SC - O(1)
 
 class Solution 
 {
@@ -25,6 +26,22 @@ public:
 };
 */
 
+/*
+Iterative DP - 
+
+TC - O(amount*coins)
+SC - O(amount)
+
+1. Starage and meaning - 1D Table of size = amount+1.
+   ith cell of DP means the number of ways of getting i amount of money with the coins considered till now.
+   
+2. Direction - small problem is number of ways of getting 0 amount of money - there will be always one way pick no coins.
+    Large problem is getting required amount of money.
+
+3. Travel and solve - Traverse from 0 to amount. Consider one coin at each time. After each iteration dp state represents the number of ways considering all coins till now.
+
+Here, The problem is of finding combination and not of permutations i.e. either 2 5 or 5 2 can be considered for amount 7 bt not both. This is ensured by our loop - it will always first pick the coin which appears first in the coins array.
+*/
 class Solution 
 {   
 public:
@@ -33,11 +50,11 @@ public:
         vector<int> dp(amount+1,0);
         
         dp[0]=1;
-        for(int i=0;i<coins.size();i++)
+        for(int coin : coins)
         {
-            for(int j=coins[i];j<=amount;j++)
+            for(int j=coin;j<=amount;j++)
             {
-                    dp[j]+=dp[j-coins[i]];
+                    dp[j]+=dp[j-coin];
             }
         }
         return dp[amount];
