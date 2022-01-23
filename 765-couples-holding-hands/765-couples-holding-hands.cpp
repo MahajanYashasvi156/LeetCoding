0ccf6973db947ec704be59b1ddd379a8dc1c00dc
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-01-23 14:30:33 +0530

    Time: 6 ms (14.42%), Space: 7.3 MB (78.85%) - LeetHub

---
+++ 765-couples-holding-hands/765-couples-holding-hands.cpp
@@ -0,0 +1,34 @@
+class Solution 
+{
+public:
+    int minSwapsCouples(vector<int>& row) 
+    {
+        int count =0;
+        int n = row.size();
+        for(int i=0;i<n;i=i+2)
+        {
+            int toFind ;
+            int index;
+            if(row[i]%2==0)
+                toFind=row[i]+1;
+            else
+                toFind = row[i]-1;
+            
+            for(int j=i+1;j<n;j++)
+            {
+                if(row[j]==toFind)
+                {
+                    index=j;
+                    break;
+                }  
+            }
+            if(index!=i+1)
+            {
+                swap(row[i+1],row[index]);
+                 count++;
+            }
+               
+        }
+        return count;
+    }
+};
