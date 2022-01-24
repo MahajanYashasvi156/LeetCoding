02348a8fa07c75eeb35669bb473c448a394b8e80
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-01-24 19:31:18 +0530

    Time: 35 ms (36.79%), Space: 13.8 MB (61.73%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -0,0 +1,61 @@
+class Solution 
+{
+int findParent(vector<int> &dsuf,int x)
+    {
+        if(dsuf[x]==-1)
+            return x;
+        return dsuf[x]=findParent(dsuf,dsuf[x]);
+    }
+    
+    bool unionSet(vector<int> &dsuf,vector<int> &rank ,int x,int y)
+    {
+        int xpar = findParent(dsuf,x);
+        int ypar = findParent(dsuf,y);
+        
+        if(xpar==ypar)
+            return true;
+        if(xpar!=ypar)
+        {
+           if(rank[xpar]>rank[ypar])
+           {
+                dsuf[ypar]=xpar;
+           }
+            else if(rank[ypar]>rank[xpar])
+           {
+                dsuf[xpar]=ypar;
+           }
+            else
+            {
+                dsuf[ypar]=xpar;
+                rank[xpar]+=1;
+            }
+        }
+        return false;
+    }
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) 
+    {
+        int n = isConnected.size();
+        vector<int> dsuf(n,-1);
+        vector<int> rank(n,0);
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(isConnected[i][j]==1)
+                {
+                     unionSet(dsuf,rank,i,j);
+                }
+            }
+        }
+        int components=0;
+        for(int i=0;i<n;i++)
+        {
+            if(dsuf[i]==-1)
+                components++;
+        }
+        return components;
+        
+    }
+};
