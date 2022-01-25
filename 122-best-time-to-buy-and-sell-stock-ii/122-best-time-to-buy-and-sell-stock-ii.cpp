5dc76c204101e3e807b957c95ca7c88446bd25a6
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-01-25 16:22:44 +0530

    Time: 15 ms (13.33%), Space: 12.9 MB (99.37%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,3 +1,9 @@
+/*
+Link - https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/submissions/
+
+TC - O(n)
+SC - O(1)
+*/
 class Solution 
 {
 public:
@@ -7,18 +13,19 @@ public:
         int maxProfit =0;
         int buy=0;
         int sell=0;
+        
         for(int i=1;i<n;i++)
         {
             if(prices[i]>=prices[i-1])
                 sell++;
             else
             {
-                maxProfit+=(prices[sell]-prices[buy]);
+                maxProfit+=prices[sell]-prices[buy];
                 sell=i;
                 buy=i;
             }
             if(i==n-1)
-                maxProfit+=(prices[sell]-prices[buy]);
+                maxProfit+=prices[sell]-prices[buy];
         }
         return maxProfit;
     }
