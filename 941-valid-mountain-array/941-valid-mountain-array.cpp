feb4e69e55ec297385c4f95bf2b388dcb5500693
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-01-25 13:09:52 +0530

    Time: 38 ms (39.87%), Space: 22.5 MB (48.39%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -1,3 +1,14 @@
+
+/*
+Link - https://leetcode.com/problems/valid-mountain-array/submissions/
+
+TC - O(N)
+SC - O(1)
+
+Peak element cant be starting or ending element.
+Before peak element all elements should in increasing order.
+After peak element all elememts should be in decreasing order.
+*/
 class Solution 
 {
 public:
