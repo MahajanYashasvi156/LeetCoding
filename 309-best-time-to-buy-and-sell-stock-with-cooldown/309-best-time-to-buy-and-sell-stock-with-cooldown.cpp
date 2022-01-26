40333bd838a7f9d59f6ec214016fb8a2283b3549
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-01-26 12:51:25 +0530

    Time: 7 ms (42.49%), Space: 11.1 MB (81.08%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,3 +1,13 @@
+/*
+Link - https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/submissions/
+TC - O(N)
+SC - O(1)
+
+Approach - This is similar to the fees que, there we were refering to last day's states
+Here, for selling we will refer to last day's states
+But for buying, we will take yesterday's prev day's sell prof as last day is cooldown day, so we can't buy today if we have sold yesterday
+*/
+
 class Solution 
 {
 public:
