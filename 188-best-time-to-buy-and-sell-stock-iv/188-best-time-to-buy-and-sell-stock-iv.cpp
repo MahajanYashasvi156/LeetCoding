a38a46f1cbf773e8989f646b090510581426e92c
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-01-26 16:07:18 +0530

    Time: 6 ms (76.58%), Space: 11.7 MB (44.86%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,7 +1,7 @@
 /*
-Link - 
+Link - https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/submissions/
 
-*/
+
 class Solution 
 {
 public:
@@ -25,6 +25,32 @@ public:
                 }
                 dp[trans][day]=m;
                 
+            }
+        }
+        return dp[k][n-1];
+    }
+};
+*/
+
+class Solution 
+{
+public:
+    int maxProfit(int k, vector<int>& prices) 
+    {
+        int n = prices.size();
+        
+        if(n<=1)
+            return 0;
+        
+        vector<vector<int>> dp(k+1,vector<int>(n,0));
+        
+        for(int trans=1;trans<=k;trans++)
+        {
+            int prevmax=-1*prices[0];
+            for(int day=1;day<n;day++)
+            {
+               dp[trans][day]=max(dp[trans][day-1],prices[day]+prevmax);
+               prevmax=max(prevmax,dp[trans-1][day]-prices[day]);
             }
         }
         return dp[k][n-1];
