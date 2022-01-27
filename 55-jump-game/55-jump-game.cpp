56571e42f9d3c7af2296aea6571d7a6431877b6b
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-01-27 14:07:52 +0530

    Time: 736 ms (9.50%), Space: 48.8 MB (14.03%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -15,7 +15,7 @@ public:
         dp[n-1]=true;
         for(int i=n-2;i>=0; i--)
         {
-            for(int j=i+1;j<=i+nums[i] ;j++)
+            for(int j=i+1;j<=i+nums[i] && j<n ;j++)
             {
                 if(dp[j]==true)
                 {
