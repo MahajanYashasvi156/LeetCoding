54d1ae5970f1063fb8eeae833edce7ec95f76379
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-01-28 21:57:12 +0530

    Time: 147 ms (68.35%), Space: 9.1 MB (43.74%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,11 +1,14 @@
+/*
+Link - https://leetcode.com/problems/perfect-squares/
+TC - O(N*root(N))
+SC - O(N)
+*/
 class Solution 
 {
 public:
     int numSquares(int n) 
     {
         vector<int> dp(n+1);
-        dp[0]=0;
-      
         int count = 0;
         for(int i=1;i<=n;i++)
         {
