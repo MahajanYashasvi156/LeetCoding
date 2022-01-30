9a0b73342bb43c35213a2ca0860962525f7764d5
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-01-30 14:07:45 +0530

    Time: 44 ms (37.09%), Space: 25 MB (81.49%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,4 +1,7 @@
-class Solution {
+
+/*
+class Solution 
+{
 public:
     void rotate(vector<int>& nums, int k) 
     {
@@ -9,3 +12,18 @@ public:
              nums[(i+k)%n]=temp[i];
     }
 };
+*/
+
+class Solution 
+{
+public:
+    void rotate(vector<int>& nums, int k) 
+    {
+        int n = nums.size();
+        k = k%n;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+    }
+};
+
