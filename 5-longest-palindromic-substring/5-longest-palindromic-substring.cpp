0b1d648ff855fec8207043df366c12caacddfd7d
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-02-01 16:32:39 +0530

    Time: 572 ms (19.00%), Space: 30.1 MB (37.21%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,4 +1,11 @@
-class Solution {
+/*
+Link - https://leetcode.com/problems/longest-palindromic-substring/submissions/
+
+TC - O(N^2)
+SC - O(N^2)
+
+class Solution 
+{
 public:
     string longestPalindrome(string s) 
     {
@@ -30,10 +37,55 @@ public:
                         dp[i][j]=max(dp[i+1][j],dp[i][j-1]);
                 
                 }
-                
                 if(maxLength<dp[i][j])
                 {
                     maxLength = dp[i][j];
+                    start = i;
+                }
+            }
+        }
+        return s.substr(start,maxLength);
+    }
+};
+*/
+
+class Solution 
+{
+public:
+    string longestPalindrome(string s) 
+    {
+        int n = s.size();
+        vector<vector<bool>>dp(n,vector<bool>(n));
+        
+        int maxLength = 0;
+        int start = 0;
+        
+        for(int l = 1 ; l<=n;l++)
+        {
+            for(int i = 0;i<=n-l ;i++)
+            {
+                int j = i+l-1;
+                
+                if(l==1)
+                    dp[i][j]=true;
+                
+                else if (l==2)
+                {
+                    if(s[i]==s[j])
+                        dp[i][j]=true;
+                    else
+                        dp[i][j]=false;
+                }
+                else
+                {
+                    if(s[i]==s[j] and dp[i+1][j-1])
+                        dp[i][j]=true;
+                    else
+                        dp[i][j]=false;
+                }
+                if(dp[i][j])
+                {
+                    maxLength = l;
                     start = i;
                 }
             }
