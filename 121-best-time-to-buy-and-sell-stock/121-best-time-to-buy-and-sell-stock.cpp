4bba657c46d6879e78fa45c1a42e0949ba0ef592
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-02-01 10:08:52 +0530

    Time: 157 ms (51.25%), Space: 93.3 MB (90.32%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,22 +1,15 @@
-/*
-https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-
-TC - O(N) where N is no of days. 
-SC - O(1)
-*/
-class Solution {
+class Solution 
+{
 public:
     int maxProfit(vector<int>& prices) 
     {
-        //minPrice stores the minimum price of the stock before current day.
-        int minPrice=prices[0];
-        int maxProfit=0;
-        for(int day=1;day<prices.size();day++)
+        int minPrice = prices[0];
+        
+        int maxProfit =0;
+        for(int i=1;i<prices.size();i++)
         {
-            //maxProfit stores the profit if we will sell current day and profit till now.
-            maxProfit= max(maxProfit,prices[day]-minPrice);
-            //update the minPrice of stocks till for next day. 
-            minPrice = min(minPrice,prices[day]);
+            maxProfit=max(maxProfit,prices[i]-minPrice);
+            minPrice = min(minPrice,prices[i]);
         }
         return maxProfit;
     }
