cdbeb378c5a62225d2f9e955ed1d388a86a2eeff
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-02-02 23:53:12 +0530

    Time: 182 ms (17.51%), Space: 9.7 MB (65.23%) - LeetHub

---
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -0,0 +1,33 @@
+class Solution 
+{
+public:
+    bool isMatch(string s, string p) 
+    {
+        int ssize = s.size();
+        int psize = p.size();
+        vector<vector<bool>> dp(psize+1,vector<bool>(ssize+1));
+        
+        dp[0][0]=true;
+        
+        for(int i=1;i<=psize;i++)
+        {
+            for(int j=0;j<=ssize;j++)
+            {
+                if(j==0)
+                {
+                    if(p[i-1]=='*')
+                        dp[i][j]=dp[i-1][j];
+                }
+                else if(s[j-1]==p[i-1])
+                    dp[i][j]=dp[i-1][j-1];
+                else if(p[i-1]=='?')
+                    dp[i][j]=dp[i-1][j-1];
+                else if(p[i-1]=='*')
+                    dp[i][j]=dp[i-1][j-1] || dp[i-1][j] || dp[i][j-1];
+                else 
+                    dp[i][j]=false;
+            }
+        }
+        return dp[psize][ssize];
+    }
+};
