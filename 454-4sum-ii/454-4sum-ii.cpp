3e06420350a39af9a86701dfbff2573fcc977168
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-02-03 12:58:38 +0530

    Time: 350 ms (39.25%), Space: 24.6 MB (36.00%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) 
     {
-        map<int,int> firstTwo;
+        unordered_map<int,int> firstTwo;
         int n = nums1.size();
         for(int i=0;i<n;i++)
         {
