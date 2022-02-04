7e2bfc164166ce810f5be9c969f6a9cefc9334b5
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-02-04 17:45:52 +0530

    Time: 244 ms (19.86%), Space: 84.2 MB (14.46%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -31,3 +31,29 @@ public:
         return result;
     }
 };
+
+//Can also be done by susbstituting 0 with -1 . Find the lasrgest subarray with sum 0.
+/*
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums)
+    {
+        unordered_map<int,int> freq;//count,index;
+        freq[0]=-1;
+        int count=0;
+        int maxlen=0;
+        for(int i=0;i<nums.size();i++)
+       {
+           if(nums[i]==0)
+               count++;
+            else
+                count--;
+            if(freq.find(count)!=freq.end())
+                maxlen=max(maxlen,i-freq[count]);
+            else
+                freq[count]=i;
+       }
+        return maxlen;
+    }
+};
+*/
