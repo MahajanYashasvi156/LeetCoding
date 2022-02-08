300036487405aa2cf970d2eceba245d066c3a3f8
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-02-08 23:59:44 +0530

    Time: 6 ms (14.93%), Space: 5.9 MB (75.31%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,6 +1,11 @@
 /*
 Link - https://leetcode.com/problems/add-digits/submissions/
 
+Algortihm -
+If the number is 0 then the sum of that will be 0.
+Else if number is exactly divisible by 9 then return 9.
+Else return the num%9.
+
 TC - O(1)
 SC - O(1)
 */
