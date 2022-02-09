fcf5d7af658521fc79883417b1727ecb68ef05d5
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-02-09 19:57:43 +0530

    Time: 20 ms (73.03%), Space: 13.8 MB (29.46%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,3 +1,10 @@
+/*
+Link - https://leetcode.com/problems/k-diff-pairs-in-an-array/submissions/
+
+TC - O(N)
+SC - O(N)
+*/
+
 class Solution 
 {
 public:
@@ -13,12 +20,11 @@ public:
                     kDiffUniquePair++;
                 if(mp.find(nums[i]+k)!=mp.end())
                     kDiffUniquePair++;
-      
-                mp[nums[i]]=false;
+                mp[nums[i]]=false; //means (nums[i],nums[i]) is not included.
             }
             else if(k==0 and mp[nums[i]]==false)
             {
-                mp[nums[i]]=true;
+                mp[nums[i]]=true;//means (nums[i],nums[i]) is included.
                 kDiffUniquePair++;
             }
         }
