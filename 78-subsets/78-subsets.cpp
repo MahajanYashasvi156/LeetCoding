8378e55fcd65beae6adf01137323c1a2cd505b21
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-02-14 02:04:58 +0530

    Time: 3 ms (69.97%), Space: 15.3 MB (27.81%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+void subsetSumUtility(vector<int> arr,vector<vector<int>>&res,int index,vector<int>&path)
+{
+    if(index==arr.size())
+    {
+        res.push_back(path);
+        return;
+    }
+    
+    //Pick the element at index.
+    path.push_back(arr[index]);
+    subsetSumUtility(arr,res,index+1,path);
+    path.pop_back();
+    
+    //Don't pick the element at index.
+    subsetSumUtility(arr,res,index+1,path);
+}
+    
+    vector<vector<int>> subsets(vector<int>& nums)
+    {
+        vector<vector<int>>res;
+        vector<int>path;
+        subsetSumUtility(nums,res,0,path);
+        return res;
+    }
+};
