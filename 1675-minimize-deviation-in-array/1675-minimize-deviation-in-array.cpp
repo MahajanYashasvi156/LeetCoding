f7db812dfd7b72510b3a199e701683b3a02c9451
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-02-19 16:19:33 +0530

    Time: 192 ms (92.05%), Space: 71.3 MB (58.94%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -13,7 +13,7 @@ public:
         }
         int diff = INT_MAX;
         int max_Ele = INT_MIN;
-        while(!pq.empty())
+        while(pq.size()>1)
         {
             max_Ele = pq.top();
             pq.pop();
