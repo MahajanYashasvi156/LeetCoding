6a266e866002ecadf3223432beaa0d8777c064ea
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-02-23 22:22:21 +0530

    Time: 3 ms (97.31%), Space: 9 MB (23.69%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -20,7 +20,51 @@ public:
 */
 
 
+/*
+Link - https://leetcode.com/problems/clone-graph/
 
+TC - O(V+E) //DFS
+SC - O(V)
+ASC - O(V)
+
+Appraoch - 
+Perform DFS with some modification.
+1. Instead of visited vector, maintain visited map which indicates following -
+    a) Whether the input node is created and explored or not.
+    b) Mapping of old address of the node with its cloned address.
+    
+2. Same as DFS, set visited in the beginning of the call.
+3. a)If the neighbour is in the map i.e. neighbour is created and explored then just add new address of the neighbour to the neighbout list.
+   b) Else Call DFS on the neighbour.
+*/
+class Solution 
+{
+    Node* dfs(Node* node,map<Node*,Node*> &visited)
+    {
+        Node * curr = new Node(node->val); 
+        visited[node]=curr;
+        
+        for(Node* n:node->neighbors)
+        {
+            if(visited.find(n)==visited.end())
+                curr->neighbors.push_back(dfs(n,visited));
+        
+            else
+                curr->neighbors.push_back(visited[n]);      
+        }
+        return curr;
+    }
+public:
+    Node* cloneGraph(Node* node) 
+    {
+        if(node==NULL)
+            return NULL;
+        map<Node*,Node*> visited;
+        return dfs(node,visited); 
+    }
+};
+
+/*
 class Solution 
 {
     Node* dfs(Node* src, map<Node*,Node*> &m)
@@ -46,3 +90,4 @@ public:
         return dfs(node,m);
     }
 };
+*/
