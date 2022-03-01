a9210cd7f2f8ae388991265ed76acf9656509ee0
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-03-02 01:17:23 +0530

    Time: 9 ms (11.67%), Space: 9 MB (90.61%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -4,18 +4,31 @@ Link - https://leetcode.com/problems/merge-sorted-array/
 TC - O(nlogn)
 SC - O(1)
 */
+
+/*
 class Solution 
 {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
     {
-        /*
         for(int i=0;i<n;i++)
         {
             nums1[m+i]= nums2[i];
         }
         sort(nums1.begin(),nums1.end());
-        */
+    }
+};
+*/
+
+/*
+TC - O(m+n)
+SC - O(1)
+*/
+class Solution 
+{
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+    {
         int i = m-1;
         int j = n-1;
         int k = m+n-1;//Last index of nums1 array.
