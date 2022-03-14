99879be0f71037048dc02006b26262554b0162dd
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-03-14 17:20:18 +0530

    Time: 11 ms (38.47%), Space: 10.9 MB (12.99%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -5,46 +5,39 @@ class Solution
 public:
     string simplifyPath(string path) 
     {
-        stack<string> st;
-        string res;
+        stack<string> s;
         
-        for(int i = 0;  i<path.size(); ++i)
+        for(int i = 0; i<path.size();i++)
         {
-            if(path[i] == '/')    
+            if(path[i]=='/')
                 continue;
-            string temp;
-			// iterate till we doesn't traverse the whole string and doesn't encounter the last /
-            while(i < path.size() && path[i] != '/')
+            string temp ;
+            while(i<path.size() and path[i]!='/')
             {
-				// add path to temp string
-                temp += path[i];
-                ++i;
+                temp = temp + path[i];
+                i++;
             }
-            if(temp == ".")
+            if(temp==".")
+            {
                 continue;
-			// pop the top element from stack if exists
-            else if(temp == "..")
-            {
-                if(!st.empty())
-                    st.pop();
             }
-            else
-			// push the directory file name to stack
-                st.push(temp);
+            else if(temp=="..")
+            {
+                if(!s.empty())
+                    s.pop();
+            }
+            else 
+                s.push(temp);
         }
-        
-		// adding all the stack elements to res
-        while(!st.empty())
-        {
-            res = "/" + st.top() + res;
-            st.pop();
+        string result;
+        while(!s.empty())
+        { 
+            result ="/"+ s.top() + result;
+            s.pop();  
         }
-        
-		// if no directory or file is present
-        if(res.size() == 0)
-            return "/";
-        
-        return res;
+        if(result.size()==0)
+            result="/";
+        return result;
     }
 };
 
