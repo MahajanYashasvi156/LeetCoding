9fca770a0d471bb9c83b9bad45261be8672514da
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-04-08 20:52:23 +0530

    Time: 53 ms (36.75%), Space: 19.7 MB (91.17%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -1,3 +1,54 @@
+/*
+https://leetcode.com/problems/kth-largest-element-in-a-stream/solution/
+
+TC - O(NlogN + MlogK) - M = no of calls to add
+SC - O(N)
+Approach
+Maintain a min heap of k elements as it will keep the kth largest element at root of min heap
+Then it will be O(1) oprtation to get the Kth largest element
+*/
+
+class KthLargest {
+public:
+    priority_queue<int,vector<int>,greater<int>> minHeap;
+    int kth;
+    
+    /*
+    1. Create min heap O(N)
+    2. Remove the n-k elements from minheap keeping only k largest elements O(NlogN)
+    */
+    
+    KthLargest(int k, vector<int>& nums)
+    {
+        kth=k;
+        for(int i=0;i<nums.size();i++)
+            minHeap.push(nums[i]);
+        while(minHeap.size()>kth)
+            minHeap.pop();
+    }
+    
+    /*
+    1. Push given val to min heap O(logK)
+    2. Remove the smallest element as min heap may have k+1 elements O(logK)
+    3. Get kth largest element - O(1)
+    */
+    int add(int val) 
+    {
+        minHeap.push(val);
+        if(minHeap.size()>kth)
+            minHeap.pop();
+        int kthLargestElem=minHeap.top();
+        return kthLargestElem;
+        
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
+/*
 class KthLargest 
 {
     priority_queue<int,vector<int>,greater<int>> kmin;
@@ -23,8 +74,5 @@ public:
     }
 };
 
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest* obj = new KthLargest(k, nums);
- * int param_1 = obj->add(val);
- */
+
+*/
