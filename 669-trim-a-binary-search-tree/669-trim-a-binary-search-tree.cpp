eea81dbd484f0973be33e5c0ba0d7f45c8a69a4c
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-04-17 00:00:00 +0530

    Time: 12 ms (93.37%), Space: 24 MB (7.34%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -9,6 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/*
+TC - O(n)
+SC - O(1)
+ASC - O(n)
+
+Approach - 
+if the root values is less than the low value then trimmed tree lies in the right subtree of BST.
+if the root values is greate than the high value then trimmed tree lies in the light subtree of BST.
+if the root value lies in the range then it will be the root of the trimmed tree. 
+
+LINK - https://www.youtube.com/watch?v=JNAqFV9AgVc
+LEETCODE - https://leetcode.com/problems/trim-a-binary-search-tree/submissions/
+*/
 class Solution 
 {
 public:
