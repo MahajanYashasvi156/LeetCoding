18850780224a89cbc8d1ea676c58d18d2eab5c78
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-05-18 02:13:46 +0530

    Time: 760 ms (43.21%), Space: 164 MB (23.01%) - LeetHub

---
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* result=NULL;
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) 
+    {
+        if(original == NULL or cloned == NULL)
+            return cloned;
+        if(original == target)
+        {
+            return cloned;
+        }
+         result = getTargetCopy(original->left,cloned->left,target);
+        if(result)
+            return result;
+        result = getTargetCopy(original->right,cloned->right,target);
+        return result;
+        
+    }
+};
