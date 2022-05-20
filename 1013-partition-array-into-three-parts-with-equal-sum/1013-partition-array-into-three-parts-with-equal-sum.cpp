76a7702da80152250cdd60bb1aea853b4b357b4a
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-05-20 15:16:19 +0530

    Time: 47 ms (88.35%), Space: 32.6 MB (60.09%) - LeetHub

--- 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.cpp
+++ 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.cpp
@@ -1,3 +1,5 @@
+
+/*
 class Solution {
 public:
     bool canThreePartsEqualSum(vector<int>& B) 
@@ -7,24 +9,55 @@ public:
         for(auto a : B)
             totalSum += a;
         if(totalSum%3!=0)
-            return 0;
-        
-    int oneThird = totalSum/3 ;
-    map<int,int> freq; 
-    int currSum = 0;
-    int ways = 0;
-    for(int i=0;i<A-1;i++)
-    {
-        currSum = currSum+B[i];
-
-        if(currSum == 2*oneThird and freq[oneThird]>0) // means we get the middle partition
+            return 0;   
+        int oneThird = totalSum/3 ;
+        map<int,int> freq; 
+        int currSum = 0;
+        int ways = 0;
+        for(int i=0;i<A-1;i++)
         {
-            return 1; //ways of getting first partition
+            currSum = currSum+B[i];
+            if(currSum == 2*oneThird and freq[oneThird]>0) // means we get the middle partition
+            {
+                return 1; //ways of getting first partition
+            }
+            freq[currSum]++;
         }
-        freq[currSum]++;
+        //No of ways is getting first partition and middle partition only . Third partition depends on the selection of middle partition.
+        return 0;
+        
     }
-    //No of ways is getting first partition and middle partition only . Third partition depends on the selection of middle partition.
-    return 0;
+};
+*/
+class Solution {
+public:
+    bool canThreePartsEqualSum(vector<int>& B) 
+    {
+        int A = B.size();
+        int totalSum = 0;
+        for(auto a : B)
+            totalSum += a;
+        if(totalSum%3!=0)
+            return 0;   
+        int oneThird = totalSum/3 ;
+        int currSum = 0;
+        bool flag1 = false;
+        bool flag2 = false;
+        for(int i=0;i<A-1;i++)
+        {
+            currSum = currSum+B[i];
+            if(currSum == oneThird && flag2 == false && flag1 == false)
+            {
+                flag1 = true;
+            }
+            else if(currSum == 2*oneThird && flag1 == true)
+            {
+                flag2 = true;
+            }
+            if(flag1 == true && flag2 == true)
+                return 1;
+        }
+        return 0;
         
     }
 };
