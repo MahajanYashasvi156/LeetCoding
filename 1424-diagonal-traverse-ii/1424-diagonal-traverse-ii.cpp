13e6fe99d6c1410abfda0f8ba19d63cd00a8d75d
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-05-21 19:23:13 +0530

    Time: 336 ms (59.92%), Space: 99 MB (43.65%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -1,3 +1,10 @@
+/*
+Sum of row index + column index represents to which diagonal element belongs to.
+
+Maintain map here as we dont know the number of diagonals.
+
+Traverse starting from bottom to top row and left to right columnof the nums matrix m.
+*/
 class Solution 
 {
 public:
@@ -7,7 +14,7 @@ public:
     
         for(int i =nums.size()-1;i>=0;i--)
         {
-            for(int j =nums[i].size()-1;j>=0;j--)
+            for(int j =0;j<nums[i].size();j++)
             {
                 diagonal[i+j].push_back(nums[i][j]);
             }
