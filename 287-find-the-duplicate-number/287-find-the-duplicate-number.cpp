a0b61a34f5e5765320ed07de88989fae2313f24c
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-05-23 20:23:40 +0530

    Time: 206 ms (28.04%), Space: 61.2 MB (91.66%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -14,18 +14,27 @@ public:
     }
 };
 */
+// As there is duplicate element, thus cycle present
+// Thus, this problem boils down to cycle detection of list
+// We are solving this using Tortoise method
 
 class Solution {
 public:
     int findDuplicate(vector<int>& A) 
     {
-        vector<int>count(A.size(),0);
-        for(int i =0;i<A.size();i++)
+        int slow = A[0];
+        int fast  = A[0];
+        do
         {
-            count[A[i]]++;
-            if(count[A[i]]>1)
-                return A[i];
+            slow = A[slow];
+            fast = A[A[fast]];
+        }while(slow!=fast);
+        fast= A[0];
+        while(slow!=fast)
+        {
+            slow = A[slow];
+            fast = A[fast];
         }
-        return -1;
+        return slow;
     }
 };
