0e62b3588c8ce9898174ac36b296ce6f3dfc97a1
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-05-26 03:54:02 +0530

    Time: 58 ms (42.06%), Space: 89.1 MB (97.44%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,61 +1,66 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+    double findMedianSortedArrays(vector<int>& A, vector<int>& B) 
     {
-        if(nums2.size()<nums1.size()) return findMedianSortedArrays(nums2,nums1);
-        
-        int leftElem=( nums1.size()+nums2.size()+1)/2;
-        
-        int low=0;
-        int high=nums1.size();
-        
-        int part1;
-        int part2;
-        int left1;
-        int left2;
-        int right1;
-        int right2;
-        
-        while(low<=high)
-        {
-            part1=(low+high)/2;
-            part2=leftElem-part1;
-            
-            if(part1==0)
-                left1=INT_MIN;
-            else
-                left1=nums1[part1-1];
-            if(part2==0)
-                left2=INT_MIN;
-            else
-                left2=nums2[part2-1];
-            
-            if(part1==nums1.size())
-                right1=INT_MAX;
-            else
-                right1=nums1[part1];
-            
-            if(part2==nums2.size())
-                right2=INT_MAX;
-            else
-                right2=nums2[part2];
-            
-            if(left1<= right2 and left2 <=right1)
+            int m = A.size();
+            int n = B.size();
+
+            int required = (m+n+1)/2; 
+
+            int low = 0 ;
+            int high = A.size();
+
+            while(low<=high)
             {
-                if((nums1.size()+nums2.size())%2==0)
-                    return (max(left1,left2)+min(right1,right2))/2.0;
+                int i = low+(high-low)/2;
+                int j = required-i;
+
+                if(j>n)
+                {
+                    low = i+1;
+                    continue;
+                }
+                else if(j<0)
+                {
+                    high = i-1;
+                    continue;
+                }
+
+                int left1 = INT_MIN;
+                int left2 = INT_MIN;
+                int right1 = INT_MAX;
+                int right2 = INT_MAX;
+
+                if(i>0)
+                    left1 = A[i-1];
+                if(j>0)
+                    left2 = B[j-1];
+                if(i<m)
+                    right1 = A[i];
+                if(j<n)
+                    right2 = B[j];
+
+                if(left1<=right2 and left2<=right1)
+                {
+                    if((m+n)%2 == 0)
+                    {
+                        return (max(left1,left2)+min(right1,right2))/2.0;
+                    }
+                    else //left extra one
+                    {
+                        return max(left1,left2)/1.0;
+                    }
+
+                }
+                else if(left1>right2)
+                {
+                    high = i - 1;
+                }
                 else
-                    return max(left1,left2);
-            
+                {
+                    low = i+1;
+                }
             }
-            else if (left1>right2)
-                 high=part1-1;
-            
-            else 
-                low=part1+1;
-        }
-                
-        
-       return -1;
+          return 0;
     }
 };
