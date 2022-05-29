3f1a7ed80632b48550912484c021a60c83b5e58e
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-05-29 15:36:14 +0530

    Time: 4 ms (42.58%), Space: 6 MB (32.39%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -1,7 +1,27 @@
-class Solution {
+/*
+Link - https://leetcode.com/problems/excel-sheet-column-number/submissions/
+
+TC - O(n) //Length of string
+SC - O(1)
+
+Topic - Number System.
+*/
+class Solution 
+{
 public:
     int titleToNumber(string columnTitle) 
     {
+        long int place = 1;
+        int i = columnTitle.size()-1;
+        int ans = 0;
+        while(i>=0)
+        {
+            ans = ans+ (columnTitle[i]-64)*place;
+            place = place*26;
+            i--;
+        }
+        return ans;
+        /*
         int result = 0;
         int base = 1;
         for(int i=0;i<=columnTitle[i];i++)
@@ -9,5 +29,7 @@ public:
             result = result *26 + (columnTitle[i]-'A'+1);
         }
         return result;
+        */
     }
 };
+
