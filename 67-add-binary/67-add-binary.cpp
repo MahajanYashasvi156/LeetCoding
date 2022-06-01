0699aef9b5e73c6065ff288859c1f9ec3d4b4bd5
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-01 23:14:05 +0530

    Time: 6 ms (33.69%), Space: 6.8 MB (26.74%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    string addBinary(string M, string N) 
+    string addBinary(string A, string B) 
     {
+        /*
         string res="";
         int i=M.size()-1,j=N.size()-1,c=0;
        
@@ -12,45 +13,24 @@ public:
             res+=(sum%2)+'0';
             c=sum/2;
         }
-        
+    
         reverse(res.begin(),res.end());
         return res;
-        /*
-        string result="";
-        vector<int>A;
-        vector<int>B;
-        for(auto a :M)
-            A.push_back(a-'0');
-        for(auto a :N)
-            B.push_back(a-'0');
-
+        */
+       string result="";
         int i = A.size()-1;
         int j = B.size()-1;
         int Cin = 0;
-        while(i>=0 and j >=0)
+        while(i>=0 or j >=0 or Cin) 
         {
-            result = result + to_string(A[i]^B[j]^Cin);
-            Cin = (A[i] & B[j]) | ((A[i]^B[j]) & Cin) ;
+            int a = i>=0?A[i]-'0':0;
+            int b = j>=0?B[j]-'0':0;
+            result = result + to_string(a^b^Cin);
+            Cin = (a & b) | ((a^b) & Cin) ;
             i--;
             j--;
         }
-        while(i>=0)
-        {
-            result = result + to_string(A[i]^Cin);
-            Cin = A[i] & Cin ;
-            i--;
-
-        }
-        while(j>=0)
-        {
-            result = result + to_string(B[j]^Cin);
-            Cin = B[j] & Cin;
-            j--;
-        }
-        if(Cin)
-            result+="1";
         reverse(result.begin(),result.end());
         return result;
-        */
     }
 };
