e91cb775fe41ae9d54b8b480659e5e56ee88ec89
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-01 23:52:22 +0530

    Time: 3 ms (55.13%), Space: 5.8 MB (70.32%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+public:
+    bool isPowerOfTwo(int num) 
+    {
+        
+        if(num==1)
+            return 1;
+        if(num%2 or num<1)
+            return 0;
+        int log2num = log2(num);
+        if(pow(2,log2num) == num)
+            return 1;
+        return 0;
+    }
+};
