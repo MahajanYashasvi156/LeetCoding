2626b36645aae9241f1cedfd467561e2f3accf81
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-01 19:49:45 +0530

    Time: 7 ms (47.79%), Space: 8.1 MB (31.61%) - LeetHub

---
+++ 214-shortest-palindrome/214-shortest-palindrome.cpp
@@ -0,0 +1,37 @@
+class Solution 
+{
+public:
+    string shortestPalindrome(string s) 
+    {
+        string text = s;
+        string temp = s;
+        reverse(temp.begin(),temp.end());
+        text = text + "$" + temp;
+        int i = 1;
+        int j = 0;
+        vector<int> lps(text.size(),0);
+        while(i<text.size())
+        {
+            while(j>0 and text[i]!=text[j])
+            {
+                j = lps[j-1];
+            }
+            if(text[i]==text[j])
+            {
+                lps[i] = j+1;
+                j++;
+                i++;
+            }
+            else
+            {
+                lps[i] = j;
+                i++;
+            }
+        }
+        int addingChar = s.size() - lps[text.size()-1];
+        string addedString = s.substr(s.size()-addingChar,addingChar);
+        reverse(addedString.begin(),addedString.end());
+        return   addedString+ s;
+        
+    }
+};
