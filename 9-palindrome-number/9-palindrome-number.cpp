61702d0da3fb68bb665139c91a7103b070713d2e
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-02 00:10:59 +0530

    Time: 19 ms (57.29%), Space: 5.9 MB (33.09%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -4,10 +4,20 @@ Link - https://leetcode.com/problems/palindrome-number/
 TC - O(log10(n))
 SC - O(1)
 */
-class Solution {
+class Solution 
+{
 public:
     bool isPalindrome(int x) 
     {
+        long int reversex = 0;
+        int tempx = x;
+        while(tempx>0)//to ensure negative numbers also.
+        {
+            reversex = reversex*10+tempx%10;
+            tempx/=10;
+        }
+        return reversex==x;
+        /*
         string s = to_string(x);
         int i=0;
         int j = s.size()-1;
@@ -22,5 +32,6 @@ public:
                 return false;
         }
         return true;
+        */
     }
 };
