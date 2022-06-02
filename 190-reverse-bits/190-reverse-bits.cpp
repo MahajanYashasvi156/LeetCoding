37ed2885cb3f46b84d76b58cd8fb282c3c45edcd
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-03 01:24:09 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (51.12%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -3,6 +3,7 @@ class Solution
 public:
     uint32_t reverseBits(uint32_t A) 
     {
+        /*
         uint32_t reverseA = 0;
         for(int i =0;i<32;i++)
         {
@@ -11,5 +12,28 @@ public:
             A = A>>1;
         }
         return reverseA;
+        */
+        int low = 0;
+        int high = 31;
+        while(low<high)
+        {
+            int x = 0;
+            int y = 0;
+
+            if(A & 1<<low)
+                x = 1;
+            if(A & 1<<high)
+                y=1;
+
+            if(x^y)
+            {
+                A = A ^ (1<<low);
+                A = A ^ (1<<high);
+            }
+
+            low++;
+            high--;
+        }
+        return A;
     }
 };
