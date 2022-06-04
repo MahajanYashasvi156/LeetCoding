239d5bf8a191db7566fb6a67833f503fde9f784c
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-04 15:18:44 +0530

    Time: 2 ms (46.28%), Space: 5.8 MB (98.57%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,27 +1,28 @@
+//https://www.geeksforgeeks.org/binomial-coefficient-dp-9/
 class Solution 
 {
     int binomialCoeff(int n, int k)
-{
-    int C[n + 1][k + 1];
-    int i, j;
- 
-    // Calculate value of Binomial Coefficient
-    // in bottom up manner
-    for (i = 0; i <= n; i++) {
-        for (j = 0; j <= min(i, k); j++) {
-            // Base Cases
-            if (j == 0 || j == i)
-                C[i][j] = 1;
- 
-            // Calculate value using previously
-            // stored values
-            else
-                C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+    {
+        int C[n + 1][k + 1];
+        int i, j;
+
+        // Calculate value of Binomial Coefficient
+        // in bottom up manner
+        for (i = 0; i <= n; i++) {
+            for (j = 0; j <= min(i, k); j++) {
+                // Base Cases
+                if (j == 0 || j == i)
+                    C[i][j] = 1;
+
+                // Calculate value using previously
+                // stored values
+                else
+                    C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+            }
         }
+
+        return C[n][k];
     }
- 
-    return C[n][k];
-}
 public:
     int uniquePaths(int m, int n) 
     {
