82c22a35a1d6bf1375cf514d5c89fc4209de1715
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-04 15:27:19 +0530

    Time: 2 ms (46.28%), Space: 5.9 MB (73.79%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,8 +1,9 @@
-//https://www.geeksforgeeks.org/binomial-coefficient-dp-9/
 /*
+//https://www.geeksforgeeks.org/binomial-coefficient-dp-9/
+
    C(n, k) = C(n-1, k-1) + C(n-1, k)
    C(n, 0) = C(n, n) = 1
-*/
+
 class Solution 
 {
     int binomialCoeff(int n, int k)
@@ -35,5 +36,22 @@ public:
         
         return binomialCoeff(totalsteps,mini);
         
+    }
+};
+*/
+class Solution 
+{
+public:
+    int uniquePaths(int m, int n)
+    {
+        double totalsteps = m+n-2;
+        double mini = min(m,n)-1;
+        
+        double ans = 1.0;
+        for(int i = 0;i<mini;i++)
+        {
+            ans = ans* (totalsteps-i)/(i+1);
+        }
+        return ans;
     }
 };
