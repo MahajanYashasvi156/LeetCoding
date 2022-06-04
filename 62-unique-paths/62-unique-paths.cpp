f708692c949016a7e7bd4e1178c36526ce0903d9
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-04 15:20:37 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.79%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,4 +1,8 @@
 //https://www.geeksforgeeks.org/binomial-coefficient-dp-9/
+/*
+   C(n, k) = C(n-1, k-1) + C(n-1, k)
+   C(n, 0) = C(n, n) = 1
+*/
 class Solution 
 {
     int binomialCoeff(int n, int k)
