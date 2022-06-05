3f313b9013d9c712271651fd09dae2c51313d2b4
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-05 19:35:49 +0530

    Time: 30 ms (14.83%), Space: 18.4 MB (75.11%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,7 +1,50 @@
+class Solution 
+{
+public:
+    int removeDuplicates(vector<int>& nums) 
+    {
+        int prev = nums[0];
+        for(int i = 1;i<nums.size();i++)
+        {
+            if(nums[i]==prev)
+            {
+                nums[i]=INT_MAX;
+            }
+            else
+            {
+                prev = nums[i];
+            }
+        }
+
+        int i = 0;
+        int j = 0;
+        while(j<nums.size())
+        {
+            if(nums[j]!=INT_MAX)
+            {
+                swap(nums[i],nums[j]);
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+};
+
+
+
+
+
+
 /*
+Link - https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+TC - O(NlogN)
+SC - O(1)
+
+If the number appears more than once, replace it with INT_MAX.
+Then sort it so that valid numbers appears in the beginning.
 
 
-*/
 class Solution 
 {
 public:
@@ -26,3 +69,4 @@ public:
         return k;
     }
 };
+*/
