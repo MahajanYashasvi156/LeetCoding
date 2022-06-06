67b10e9f24e6121e81aed9809924cecf0d6b5a4e
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-06 23:28:26 +0530

    Time: 311 ms (21.27%), Space: 51 MB (54.35%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -1,3 +1,82 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution 
+{
+    ListNode* merge(ListNode* headA , ListNode* headB)
+{
+    ListNode* head = NULL;
+    ListNode* ptr1 = headA;
+    ListNode* ptr2 = headB;
+    ListNode* prev = NULL;
+    while(ptr1 or ptr2)
+    {   
+        int x = ptr1 ? ptr1->val : INT_MAX;
+        int y = ptr2 ? ptr2->val : INT_MAX;
+
+        if(x<=y)
+        {
+            if(prev)
+                prev->next = ptr1;
+            prev = ptr1;
+            ptr1 = ptr1->next;
+        }
+        else 
+        {
+            if(prev)
+                prev->next = ptr2;
+            prev = ptr2;
+            ptr2=ptr2->next;
+        }
+        if(head==NULL)
+        {
+            head = prev;
+        }
+    }
+    return head;
+}
+
+ListNode* findMid(ListNode* low)
+{
+    ListNode* prevMid = NULL;
+    ListNode* fast = low;
+    while(fast and  fast->next)
+    {
+        if(prevMid == NULL)
+            prevMid = fast;
+        else
+            prevMid = prevMid->next;
+        fast = fast->next->next;
+    }
+    ListNode* middle = prevMid->next;
+    prevMid->next = NULL;
+    return middle;
+}
+
+public:
+    ListNode* sortList(ListNode* A) 
+    {
+        if(A==NULL or A->next==NULL)
+        return A;
+
+        ListNode* middle = findMid(A);
+        ListNode* first = sortList(A);
+        ListNode* second = sortList(middle);
+        return merge(first,second);
+    }
+};
+
+
+
+
+
 /*
 https://leetcode.com/problems/sort-list/submissions/
 
@@ -11,7 +90,7 @@ MS(head)
     3. sortedSecondList = MS(mid) -- second list
     4. merger2SortedLists(sortedFirstList,sortedSecondList);
 
-*/
+
 
 class Solution 
 {
@@ -92,3 +171,4 @@ class Solution
         return mergeTwoLists(left,right);
     }
 };
+*/
