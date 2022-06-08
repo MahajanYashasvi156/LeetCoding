602913f336ddcc922dec72fc30c634ce5ce650bd
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-08 16:40:19 +0530

    Time: 12 ms (31.44%), Space: 9.2 MB (68.39%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -1,12 +1,11 @@
+
+/*
 class Solution 
 {
-    
-    
 public:
     string simplifyPath(string path) 
     {
         stack<string> s;
-       
         for(int i = 0; i<path.size();i++)
         {
             if(path[i]=='/')
@@ -41,4 +40,46 @@ public:
         return result;
     }
 };
+*/
 
+
+class Solution 
+{
+public:
+    string simplifyPath(string path) 
+    {
+        deque<string> dq;
+        for(int i = 0; i<path.size();i++)
+        {
+            if(path[i]=='/')
+                continue;
+            string temp;
+            while(i<path.size() and path[i]!='/')
+            {
+                temp +=path[i];
+                i++;
+            }
+            if(temp==".")
+            {
+                continue;
+            }
+            else if(temp=="..")
+            {
+                if(!dq.empty())
+                    dq.pop_back();
+            }
+            else 
+                dq.push_back(temp);
+        }
+
+        string result = "";
+        while(!dq.empty())
+        { 
+            result.append("/"+dq.front());
+            dq.pop_front();  
+        }
+        if(result.size()==0)
+            result="/";
+        return result;
+    }
+};
