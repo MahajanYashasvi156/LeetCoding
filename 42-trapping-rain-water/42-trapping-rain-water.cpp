18e50cfae7b18135e4f30ec1ecd852e80dabdfd1
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-10 20:38:28 +0530

    Time: 47 ms (5.25%), Space: 21.2 MB (5.35%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int trap(vector<int>& height)
+    {
+        int size=height.size();
+        vector<int> rightMax(size);
+        vector<int> leftMax;
+        int trappedWater=0;
+        //First height has not any maximum height.
+        leftMax.push_back(0);
+        //Left max stores the maximum height before current height excluding current height.
+        for(int i=1;i<size;i++)
+        {
+            leftMax.push_back(max(height[i-1],leftMax[i-1]));
+        }
+        //Last height has not any maximum height.
+        rightMax[size-1]=INT_MIN;
+         //right max stores the maximum after current height excluding current height.
+        for(int i=height.size()-2;i>=0;i--)
+        {
+            rightMax[i]=max(height[i+1],rightMax[i+1]);
+        }
+        
+        for(int i=0;i<height.size();i++)
+        {
+        //if the minimum of rightmax and left max is greater than current height then only it can trap water.Subtract that height from thr trapped water.
+            if(min(rightMax[i],leftMax[i])>height[i])
+                trappedWater=trappedWater+min(rightMax[i],leftMax[i])-height[i];
+        }
+        return trappedWater;
+    }
+};
