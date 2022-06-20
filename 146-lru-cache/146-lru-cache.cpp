d83ce81b0c2e61cd1cc00d4266f0815a585cf2b9
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-21 01:43:26 +0530

    Time: 665 ms (43.54%), Space: 174.1 MB (35.37%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,69 +1,53 @@
-class LRUCache {
+class LRUCache 
+{
+    list<pair<int,int>> queue;
+    unordered_map<int,list<pair<int,int>>::iterator> keyIndex;
+    int maxcap;
+
 public:
-    /*Two datastructures- All Operations in O(1).
-    Doubly Linked List - Stores Key, value in each node.(Cache Implementation)
-    unordered map - Key ,address of the node of the Doubly linked list.(order of keys not matters so can use unordered map instead of map)
-    */
-    list <pair<int,int>> lqueue;
-    unordered_map<int,list <pair<int,int>> ::iterator >lmap;
-    int cap;
-    
     LRUCache(int capacity) 
     {
-        cap=capacity;
-    }
-    //To access the value of the key (Time - O(1)).
-    int get(int key) 
-    {
-        //If key not exist then we have to writtern -1 value.
-        int value=-1;
-        //If key exist in the map then we have to reset the key,value pair node to the last of the queue.
-        if(lmap.find(key)!=lmap.end())
-        {
-            list <pair<int,int>> ::iterator it;
-            //Find the address of the node of the key,value pair in the queue(DLL).
-            it=lmap[key];
-            //value of the access key.
-            value=it->second;
-            //Delete this node from the current position.
-            lqueue.erase(it);
-            //Add it to the end of the List(queue).
-            lqueue.push_back({key,value});
-            //Update the address of the node in the map corresponding to the key.
-            lmap[key]=--lqueue.end();
-        }
-        return value;
+        keyIndex.clear();
+        queue.clear();
+        maxcap= capacity;
     }
     
-    //To insert the key, value in the cache - Time O(1).
-    void put(int key, int value)
+    int get(int key) 
     {
-        //If the Key not present already in the Cache.
-        if(lmap.find(key)==lmap.end())
+        if(keyIndex.find(key)!=keyIndex.end())
         {
-            //If the Cache is full.We have to evict the LRU key from the cache.
-            if(cap==lqueue.size())
-            {
-                //LRU key is the key in the first node of the List/Queue.
-                //Delete this key from the queue and map.
-                lmap.erase(lqueue.begin()->first); 
-                lqueue.pop_front(); 
-            } 
-            //Add the new key,value in the queue at the end.
-            lqueue.push_back({key,value});
-            //Add the new key with its address to the map.
-            lmap[key]=--lqueue.end();
+            int value = keyIndex[key]->second;
+            queue.erase(keyIndex[key]);
+            queue.push_back({key,value});
+            keyIndex[key] = --queue.end();
+            return value;
         }
-        //If the Key already present.
+        return -1;
+    }
+    
+    void put(int key, int value) 
+    {
+        if(keyIndex.find(key)!=keyIndex.end())
+        {
+            queue.erase(keyIndex[key]);
+        }
         else
-       {
-            //Delete the key,value pair from the queue.
-            lqueue.erase(lmap[key]);
-            //Add it to the end of the queue.
-            lqueue.push_back({key,value});
-            //Update the address of the key.
-            lmap[key]=--lqueue.end();  
-       }
-       
+        {
+            if(keyIndex.size()==maxcap)
+            {
+                int frontKey = queue.front().first;
+                keyIndex.erase(frontKey);
+                queue.pop_front();
+            }
+        }
+        queue.push_back({key,value});
+        keyIndex[key] = --queue.end();
     }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
