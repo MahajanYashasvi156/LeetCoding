9c106b504a7399627e568bab2cf4b67d20870dd8
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-28 16:55:43 +0530

    Time: 402 ms (15.19%), Space: 50.1 MB (6.98%) - LeetHub

--- 87-scramble-string/87-scramble-string.cpp
+++ 87-scramble-string/87-scramble-string.cpp
@@ -5,6 +5,31 @@ public:
     bool isScramble(string A, string B) 
     {
         int n = A.size();
+    
+        if(A==B)
+            return 1;
+        if(m.find(A+"*"+B)!=m.end())
+            return m[A+"*"+B];
+        for(int i = 1;i<n;i++)
+        {
+            int noFlip = isScramble(A.substr(0,i),B.substr(0,i)) and isScramble(A.substr(i),B.substr(i));
+            int flip = isScramble(A.substr(0,i),B.substr(n-i)) and isScramble(A.substr(i),B.substr(0,n-i));
+
+            if(noFlip or flip)
+                return  m[A+"*"+B]  = true;
+        }
+        return  m[A+"*"+B]  = false;
+    }
+};
+
+/*
+class Solution 
+{
+    unordered_map<string,bool> m;
+public:
+    bool isScramble(string A, string B) 
+    {
+        int n = A.size();
 
         if(A==B)
             return 1;
@@ -32,3 +57,4 @@ public:
         return  m[A+"*"+B]  = false;
     }
 };
+*/
