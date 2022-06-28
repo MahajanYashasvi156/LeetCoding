e315ea5e3fe2d7c3c20c9b458bb8bb7eac880a31
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-29 01:30:45 +0530

    Time: 455 ms (33.05%), Space: 10.5 MB (42.97%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -3,6 +3,51 @@ class Solution
 public:
     int lengthOfLIS(vector<int>& nums) 
     {
+        vector<int> dp(nums.size(),1);
+        for(int i = 0;i<nums.size();i++)
+        {
+            for(int j = 0;j<i;j++)
+            {
+                if(nums[j]<nums[i])
+                    dp[i] = max(dp[j]+1,dp[i]);
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Link - https://leetcode.com/problems/longest-increasing-subsequence/submissions/
+TC - O(N^2)
+SC - O(N)
+
+Meaning - Entry of ith cell of dp represents length of largest increasing subsequence ending at ith number.
+
+class Solution 
+{
+public:
+    int lengthOfLIS(vector<int>& nums) 
+    {
         int n=nums.size();
         vector<int> dp(n,1);
         for(int i=1;i<n;i++)
@@ -18,3 +63,4 @@ public:
         return *max_element(dp.begin(),dp.end());
     }
 };
+*/
