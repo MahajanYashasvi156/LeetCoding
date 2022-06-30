c7257d5012f227170873f1661fa048bc92044b71
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-30 19:15:52 +0530

    Time: 9 ms (19.92%), Space: 7.6 MB (75.60%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -6,9 +6,7 @@ public:
         int n = A[0].size();
         if(A[0][0] || A[m-1][n-1])
             return 0;
-        vector<vector<long long>> dp(m,vector<long long>(n,0));
-        dp[m-1][n-1] = 1;
-
+        
         for(int i = m-1;i>=0;i--)
         {
             for(int j = n-1;j>=0;j--)
@@ -16,13 +14,17 @@ public:
                 if(A[i][j]==0)
                 {
                     if(i<m-1)
-                         dp[i][j] = dp[i][j] + dp[i+1][j];
+                         A[i][j] = (long(A[i][j]) + A[i+1][j])%INT_MAX;
                     if(j<n-1)
-                        dp[i][j] = dp[i][j]+ dp[i][j+1];
+                        A[i][j] = (long(A[i][j])+ A[i][j+1])%INT_MAX;
+                    if(i==m-1 and j==n-1)
+                        A[i][j] = 1;
                 }
+                else
+                     A[i][j] = 0;
             }
         }
-        return dp[0][0];
+        return A[0][0];
         
     }
 };
