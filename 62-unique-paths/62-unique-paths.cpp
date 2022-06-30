6fcaddf15b5d6be2333f60ee4e8f02a76f26d470
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-30 19:01:58 +0530

    Time: 4 ms (28.01%), Space: 6.6 MB (25.35%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,3 +1,27 @@
+class Solution 
+{
+public:
+    int uniquePaths(int m, int n) 
+    {
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        dp[m-1][n-1] = 1;
+
+        for(int i = m-1;i>=0;i--)
+        {
+            for(int j = n-1;j>=0;j--)
+            {
+                if(i<m-1)
+                     dp[i][j] = dp[i][j] + dp[i+1][j];
+                if(j<n-1)
+                    dp[i][j] = dp[i][j]+ dp[i][j+1]; 
+            }
+        }
+        return dp[0][0];
+    }
+};
+
+
+
 /*
 //https://www.geeksforgeeks.org/binomial-coefficient-dp-9/
 
@@ -39,6 +63,7 @@ public:
     }
 };
 */
+/*
 class Solution 
 {
 public:
@@ -56,3 +81,4 @@ public:
         return ans;
     }
 };
+*/
