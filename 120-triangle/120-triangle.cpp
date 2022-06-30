839cf02ccbd47887a0c8431bb91febb7eec2c093
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-06-30 14:07:04 +0530

    Time: 12 ms (34.72%), Space: 8.7 MB (64.78%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+public:
+    int minimumTotal(vector<vector<int>>& A) 
+    {
+        int n=A.size();
+    
+        for(int i = n-2;i>=0;i--)
+        {
+            for(int j = 0;j<A[i].size();j++)
+            {
+                A[i][j]+=min(A[i+1][j+1],A[i+1][j]);
+            }
+        }
+        return A[0][0];
+    }
+};
