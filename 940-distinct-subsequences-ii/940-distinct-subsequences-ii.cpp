2883b1f09e153fe351e5a82da6375d1dd8529348
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-02 16:10:44 +0530

    Time: 18 ms (18.90%), Space: 7.3 MB (15.09%) - LeetHub

--- 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
+++ 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
@@ -1,4 +1,32 @@
-class Solution {
+class Solution 
+{
+public:
+    int distinctSubseqII(string s) 
+    {
+        unordered_map<char,int> last;
+        
+        vector<long>dp(s.size()+1);
+        dp[0] = 1;
+        
+        for(int i = 0;i<s.size();i++)
+        {
+            dp[i+1] = 2*dp[i]%1000000007;
+            
+            if(last.find(s[i])!=last.end())    
+            {
+                if(last[s[i]]>=0)
+                    dp[i+1]=(dp[i+1]-dp[last[s[i]]]+1000000007)%1000000007;
+            }
+
+            last[s[i]] = i;
+        }
+        return (dp[s.size()]-1+1000000007)%1000000007;
+    }
+};
+
+/*
+class Solution 
+{
 public:
     int distinctSubseqII(string s) 
     {
@@ -22,3 +50,4 @@ public:
         return (dp[s.size()-1]+1000000007)%1000000007;
     }
 };
+*/
