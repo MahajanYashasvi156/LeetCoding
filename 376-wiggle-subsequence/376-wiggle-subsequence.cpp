b3eaf6d1874c562a170b71d20c450a0fb00144a6
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-03 23:31:55 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (75.45%) - LeetHub

---
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -0,0 +1,17 @@
+/*
+https://leetcode.com/problems/wiggle-subsequence/discuss/2229495/C%2B%2B-O-(-N-)-oror-EXPLAINED-oror
+*/
+class Solution 
+{
+public:
+    int wiggleMaxLength(vector<int>& nums) 
+    {
+        int size=nums.size(), peak=1, valley=1;
+        for(int i=1; i<size; ++i){
+                 if(nums[i]>nums[i-1]) peak = valley + 1;
+            else if(nums[i]<nums[i-1]) valley = peak + 1;
+        }
+        return max(peak , valley );
+            
+    }
+};
