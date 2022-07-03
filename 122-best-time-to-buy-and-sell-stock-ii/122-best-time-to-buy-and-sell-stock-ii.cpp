06f139c39fb8ce0cdc67bb7cf9666a50fde3175e
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-03 14:16:07 +0530

    Time: 10 ms (59.04%), Space: 13 MB (88.45%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,3 +1,20 @@
+class Solution 
+{
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int profit = 0;
+        for(int i = 1;i<prices.size();i++)
+        {
+            if(prices[i]-prices[i-1]>0)
+            {
+                profit+=(prices[i]-prices[i-1]);
+            }
+        }
+        return profit;
+    }
+};
+
 /*
 Link - https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/submissions/
 TC - O(n)
@@ -5,7 +22,7 @@ SC - O(1)
 
 Sum of all profits achievable following sequence : BSBSBSBS...
 */
-
+/*
 class Solution 
 {
 public:
@@ -31,7 +48,7 @@ public:
         return maxProfit;
     }
 };
-
+*/
 /*
 class Solution 
 {
