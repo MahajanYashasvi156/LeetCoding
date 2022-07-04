05936b0e80d77c75c4e0b0cd1095a23ffc61d9d1
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-04 09:45:37 +0530

    Time: 30 ms (62.78%), Space: 18.4 MB (13.38%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -10,17 +10,13 @@ public:
         for(int i = 1;i<n;i++)
         {
             if(ratings[i]>ratings[i-1])
-            {
                 leftNeighbour[i] = leftNeighbour[i-1]+1;
-            }
         }
         
         for(int i = n-2;i>=0;i--)
         {
             if(ratings[i]>ratings[i+1])
-            {
                 rightNeighbour[i] = rightNeighbour[i+1]+1;
-            }
         }
         for(int i = 0;i<n;i++)
         {
