a8ed5e128999936eb745665585e8f964c06817a9
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-08 12:19:04 +0530

    Time: 702 ms (5.02%), Space: 17.1 MB (97.06%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,5 +1,61 @@
 class Solution 
 {
+    int countCells(vector<vector<int> > &A,int i,int j,bool &blue,bool &red,vector<vector<bool>> &visited)
+{
+    if(i==0 ||j==0)
+        blue = true;
+    if(i==A.size()-1 || j==A[0].size()-1)
+        red = true;
+    
+    if(red and blue)
+        return 1;
+    if(visited[i][j])
+        return 0;
+    visited[i][j] = 1;
+    
+    int ans = 0;
+    if(i>0 and A[i-1][j]<=A[i][j])
+        ans = countCells(A,i-1,j,blue,red,visited);
+        
+    if(!ans and  i<A.size()-1 and A[i+1][j]<=A[i][j])
+        ans = countCells(A,i+1,j,blue,red,visited);
+    
+    
+    if(!ans and  j>0 and A[i][j-1]<=A[i][j])
+        ans = countCells(A,i,j-1,blue,red,visited);
+        
+    if(!ans and j<A[0].size()-1 and  A[i][j+1]<=A[i][j])
+        ans = countCells(A,i,j+1,blue,red,visited);
+    
+    visited[i][j] = 0;
+    return ans;
+} 
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& A) 
+    {
+        int m = A.size();
+        int n = A[0].size();
+        vector<vector<bool>> visited(m,vector<bool>(n,false));
+
+        vector<vector<int>> result;
+        for(int i = 0;i<m;i++)
+        {
+            for(int j = 0;j<n;j++)
+            {
+                bool blue = false;
+                bool red = false;
+                if(countCells(A,i,j,blue,red,visited))
+                    result.push_back({i,j});
+            }
+        }
+        return result;
+    }
+};
+
+
+/*
+    class Solution 
+{
         void countCells(vector<vector<int> > &A,vector<vector<bool>>&visited,int i,int j)
     {
         if(visited[i][j])
@@ -51,3 +107,4 @@ public:
         return result;
     }
 };
+*/
