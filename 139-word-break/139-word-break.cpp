9314f3581eff1fc13d020c14e8e7fe5ec6a4c40d
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-11 16:33:10 +0530

    Time: 34 ms (29.46%), Space: 17.7 MB (11.33%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) 
+    {
+        unordered_map<string,bool> dict;
+        for(auto s:wordDict)
+            dict[s] = true;
+    
+        vector<bool> dp(s.size(),false);
+       
+        for(int i = 0;i<s.size();i++)
+        {
+            for(int j = i;j>=0;j--)
+            {
+                if(dict[s.substr(0,i+1)] or (dp[j] and dict[s.substr(j+1,i-j)]))
+                {
+                    dp[i] = true;
+                    break;
+                }   
+            }
+        }
+        return dp[s.size()-1];
+    }
+};
