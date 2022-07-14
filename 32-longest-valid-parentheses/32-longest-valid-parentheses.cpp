e7cfae35f23b13a8f8e4a938e8b404c5072a753f
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-14 18:28:35 +0530

    Time: 3 ms (90.37%), Space: 7.3 MB (38.57%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -32,3 +32,28 @@ public:
         
     }
 };
+
+/*
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        int result = 0;
+        st.push(-1);
+        for(int i =0;i<s.size();i++)
+        {
+            if(s[i]==')' and st.size()>1 and s[st.top()]=='(')
+            {
+                st.pop();
+                result = max(result,i-st.top()); 
+            }
+            else
+            {
+                st.push(i);
+            }
+        }
+        return result;
+    }
+};
+
+*/
