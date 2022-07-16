b8076340124237804f45b46049f71f1da4571236
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-16 16:22:02 +0530

    Time: 443 ms (54.17%), Space: 65.3 MB (59.35%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,3 +1,11 @@
+/*
+Intuition - Try to pick the number which allows to make as many 1's from the left side(MSB).
+
+32 31 30 ______ 3 2 1
+
+we try to make 1 on the 32th bit, if not possible then we are bound to pick elements which can make 0 at 32th bit of the maximum xor result. 
+we try to make 1 on the 31th bit, and so on.
+*/
 class Trie
 {
     Trie* node[2];
@@ -24,15 +32,15 @@ class Solution
 {
     void insert(Trie* root,int n)
     {
+        //All elements will start from the root node.
         Trie* node = root;
         for(int i = 31;i>=0;i--)
         {
             int bit = (n>>i) & 1;
 
             if(node->containsKey(bit)==false)
-            {
                 node->put(bit,new Trie());
-            }
+        
             node = node->get(bit);
         }
     }
@@ -47,11 +55,13 @@ class Solution
             int bit = num>>i & 1;
             int oppositeBit = bit ^ 1;
             
+            //If opposite bit present in the current refrence trie then the xor bit will be 1.
             if(node->containsKey(oppositeBit))
             {
                 node = node->get(oppositeBit);
                 maxXor = 1<<i | maxXor;
             }
+            //If opposite bit not present then the current xor bit of maxXor will not be set and we are bound to go with the number with the same bit only.
             else
                 node = node->get(bit);
         }
@@ -63,14 +73,14 @@ public:
         Trie* root = NULL;
         root = new Trie();
         
+        //Insert all the elements in the Trie in 32- bits.
         for(int n:nums)
             insert(root,n);
         
         int result = 0;
+        //Find the max xor of an element with all elements of the array.
         for(int n:nums)
-        {
             result = max(result,getMaxXOR(root,n));
-        }
         
         return result;
     }
