d9bbe781100808ae693e94a26d2070fb47c537d3
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-17 14:29:18 +0530

    Time: 10 ms (87.36%), Space: 11.7 MB (43.75%) - LeetHub

---
+++ 386-lexicographical-numbers/386-lexicographical-numbers.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+    void dfs(int num,int n, vector<int>&ans)
+    {
+        if(num>n)
+            return ;
+        ans.push_back(num);
+        
+        for(int i = 0;i<=9;i++)
+        {
+             dfs(num*10+i,n,ans);
+        }
+    }
+public:
+    vector<int> lexicalOrder(int n)
+    {
+        vector<int> ans;
+        for(int i = 1;i<=9;i++)
+            dfs(i,n,ans);
+        return ans;
+    }
+};
