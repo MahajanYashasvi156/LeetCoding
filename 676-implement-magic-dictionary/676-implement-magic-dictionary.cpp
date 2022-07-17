fedfbc51e9534fbd281745a3ef2b3a66c3e80ddf
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-17 16:10:14 +0530

    Time: 290 ms (54.28%), Space: 125.6 MB (32.33%) - LeetHub

--- 676-implement-magic-dictionary/676-implement-magic-dictionary.cpp
+++ 676-implement-magic-dictionary/676-implement-magic-dictionary.cpp
@@ -29,22 +29,29 @@ class Trie
     
         bool search(int i,string s,bool replaced,Node* node)
         {
+            //If at last refrence node and endwith flag is set. Also if we reached at this refrence node changing exactly one character then only we found the word. 
             if(i==s.size())
-            {
                 return (node->flag and replaced);
-            }
+            
+            //If the current character is not present in the current refrence node and we can't replace any more character then search fails.
             if(node->links[s[i]-'a']==NULL and replaced)
-            {
                 return false;
-            }
+    
+            /*
+            If till now there is no replacement and current char exist in refrence node then we have two option -
+            
+            1. Either we does not perform replacement and can pick the matched character refrence node .
+            2. Else we can replace current character with other refrences character present in the current refrence node.
+            */
+            
             if(node->links[s[i]-'a'] and search(i+1,s,replaced,node->links[s[i]-'a']))
                     return true;
             
             if(replaced==false)
             {
-                for(int j =0;j<26;j++)
+                for(int j = 'a';j<='z';j++)
                 {
-                    if(('a'+j)!=s[i] and node->links[j] and search(i+1,s,true,node->links[j]))
+                    if(j!=s[i] and node->links[j-'a'] and search(i+1,s,true,node->links[j-'a']))
                         return true;
                 }
             }
