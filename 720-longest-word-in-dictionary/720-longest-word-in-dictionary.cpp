2d6a43b7548b2670a97127d7d8301454556183ee
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-17 16:27:12 +0530

    Time: 118 ms (33.53%), Space: 31.8 MB (35.03%) - LeetHub

---
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.cpp
@@ -0,0 +1,71 @@
+class Node
+{
+    public:
+        Node* links[26];
+        bool flag = false;
+};
+class Trie
+{
+    Node* node ;
+    public:
+    Trie()
+    {
+        node = new Node();
+    }
+    
+    void insert(string s)
+    {
+        Node* curr = node;
+        
+        for(int i = 0;i<s.size();i++)
+        {
+            if(curr->links[s[i]-'a']==NULL)
+               curr->links[s[i]-'a'] = new Node();
+            curr = curr->links[s[i]-'a'];
+        }
+        
+        curr->flag = true;
+    }
+    
+    bool search(string s)
+    {
+        Node* curr = node;
+        for(int i = 0;i<s.size();i++)
+        {
+            if(curr->links[s[i]-'a']==NULL)
+                return false;
+            curr = curr->links[s[i]-'a'];
+            if(curr->flag==false)
+                return false;
+        }
+        
+        return true;
+    }
+};
+
+class Solution 
+{
+public:
+    string longestWord(vector<string>& words) 
+    {
+        Trie trie;
+        
+        for(string s:words)
+            trie.insert(s);
+        
+        string result = "";
+        
+        for(string s:words)
+        {
+            if(s.size()>result.size() and trie.search(s))
+            {
+                result = s;
+            }
+            if(s.size()==result.size() and result.compare(s)>0 and trie.search(s))
+            {
+                result = s;
+            }
+        }
+        return result;
+    }
+};
