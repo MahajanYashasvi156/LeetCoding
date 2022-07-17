655735e19e8485a05ec553e64e37f7f02c2a522f
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-17 15:33:23 +0530

    Time: 0 ms (100.00%), Space: 8 MB (79.78%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -11,9 +11,12 @@ public:
        
         dp[0] = 1;
         
+        //dp[i] = 1 means there exist substr from 0 to i-1 is valid.
+        
         for(int i = 1;i<=s.size();i++) // at i-1 last word ends.
         {
-            for(int j = i-1;j>=0 and j>=i-20;j--)//start of the last word , last word length can be 20 only. i-j <=20 for a valid word.
+            //last word length can be 20 only. i-j <=20 for a valid word.
+            for(int j = i-1;j>=0 and j>=i-20;j--)//start of the last word  
             {
                 if(dp[j] and dict[s.substr(j,i-j)])
                 {
