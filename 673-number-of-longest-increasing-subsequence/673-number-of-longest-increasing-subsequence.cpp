b21507086ece179f87eba76efd5a1bf99141206a
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-29 17:54:17 +0530

    Time: 181 ms (79.07%), Space: 13.3 MB (50.13%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
@@ -1,28 +1,23 @@
 class Solution 
-{
-//     1 2 3 4 7
-//         1 2 3 5 7
-//         1 2 4 5 7
-        
-        
-//     [1,2,4,3,5,4,7,2]
-// len - 1 
-        
+{    
 public:
     int findNumberOfLIS(vector<int>& nums) 
     {
+        //length[i] : maximum length of the LIS ending at ith index.
+        //count[i] : no of ways of getting maximal LIS ending at ith index.
         vector<int> length(nums.size(),0);
         vector<int> count(nums.size(),0);
         
-        length[0] = 1;
-        count[0] = 1;
-        
-        for(int i = 1;i<nums.size();i++)
+        for(int i = 0;i<nums.size();i++)
         {
-            int c = 1;
+            //If there would no elemnt which is smaller than current elememt then also it will make length 1 LIS by not taking any previous elemnt that's the one way.
+            //c : no of ways of picking LIS from the previous elemnts.
+            //maxi : length of the LIS from the previous elemnts.
+            int c = 1; 
             int maxi = 0;
             for(int j = 0;j<i;j++)
             {
+                //if the previous element is smaller than current elemnt then only it can be the part of LIS ending at current element.
                 if(nums[j]<nums[i])
                 {
                     if(maxi<length[j])
@@ -34,8 +29,7 @@ public:
                     {
                         c = c+count[j];
                     }
-                }
-                
+                } 
             }
             length[i] = maxi+1;
             count[i] = c;
@@ -45,11 +39,8 @@ public:
         int ans = 0;
         for(int i = 0;i<nums.size();i++)
         {
-            cout<<length[i]<<" "<<count[i]<<endl;
             if(length[i]==maxLength)
-            {
                 ans+=count[i];
-            }
         }
         return ans;
     }
