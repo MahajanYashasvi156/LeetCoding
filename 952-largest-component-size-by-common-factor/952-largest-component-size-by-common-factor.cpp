799071d289604ec809fae48829ca374fb86639b7
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-29 09:44:20 +0530

    Time: 372 ms (84.82%), Space: 51.3 MB (83.33%) - LeetHub

---
+++ 952-largest-component-size-by-common-factor/952-largest-component-size-by-common-factor.cpp
@@ -0,0 +1,51 @@
+class Solution 
+{
+    int findParent(int x,vector<int>&parent)
+    {
+        if(parent[x]==x)
+            return x;
+        return parent[x] = findParent(parent[x],parent);
+    }
+    
+    void unionFind(int x,int y ,vector<int>&parent)
+    {
+        int absoluteParentX = findParent(x,parent);
+        int absoluteParentY = findParent(y,parent);
+        
+        if(absoluteParentX!=absoluteParentY)
+        {
+            parent[absoluteParentX] = absoluteParentY;
+        }
+            
+    }
+public:
+    int largestComponentSize(vector<int>& nums) 
+    {
+        int n = *max_element(nums.begin(),nums.end());
+        
+        vector<int> parent(n+1,-1);
+        for(int i = 0;i<=n;i++)
+            parent[i] = i;
+        
+        for(int num : nums)
+        {
+            for(int j = 2;j*j<=num;j++)
+            {
+                if(num%j==0)
+                {
+                    unionFind(num,j,parent);
+                    unionFind(num,num/j,parent);
+                }
+                
+            }
+        }
+        int ans = 0;
+        unordered_map<int,int> freq;
+        for(int i = 0;i<nums.size();i++)
+        {
+            freq[findParent(nums[i],parent)]++;
+            ans = max(ans,freq[findParent(nums[i],parent)]);
+        }
+        return ans;
+    }
+};
