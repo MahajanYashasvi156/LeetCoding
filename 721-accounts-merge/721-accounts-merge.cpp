7a0ac399c2c4858d1697993db59d774cea403a18
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-30 00:40:58 +0530

    Time: 110 ms (92.06%), Space: 31.2 MB (93.93%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.cpp
+++ 721-accounts-merge/721-accounts-merge.cpp
@@ -20,35 +20,40 @@ class Solution
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) 
     {
-        unordered_map<string,int> mailId;
+        //Assign unique id to each account from 0 to account.size()-1.
         
+        //Insert all the email id with their corresponding account no.
+        unordered_map<string,int> mailId;
+        //Initially all accounts are separate.
         vector<int> parent(accounts.size(),-1); 
+        
         for(int i = 0;i<accounts.size();i++)
         {
             for(int j = 1;j<accounts[i].size();j++)
             {
+                //If the mailId is already a part of some other account then unify that old account and current account.
                 if(mailId.find(accounts[i][j])!=mailId.end())
-                {
                     unionFind(i,mailId[accounts[i][j]],parent);
-                }
                 else
-                {
-                     mailId[accounts[i][j]] = i;
-                }
+                    //just assign curr account no to the mailID.
+                     mailId[accounts[i][j]] = i; 
             }
         }
+        //This map will handle duplicacy.
         unordered_map<int, vector<string>> m;
         
+        //combining all emailid based on their absolute parent.
         for(auto a: mailId)
-        {
             m[findParent(a.second,parent)].push_back(a.first);
-        }
+    
         vector<vector<string>> result;
-        
         for(auto &a:m)
         {
+            //sort the email id's
             sort(a.second.begin(),a.second.end());
+            //insert the account name in the account detail.
             a.second.insert(a.second.begin(),accounts[a.first][0]);
+            //insert account detail
             result.push_back(a.second);
         }
         return result;
