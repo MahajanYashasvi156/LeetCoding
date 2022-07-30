753294c5d14c83aa57a96f6cd515706ddf9c1794
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-07-30 12:51:12 +0530

    Time: 956 ms (13.71%), Space: 168.8 MB (13.93%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) 
+    {
+        vector<unordered_map<char,int>>freq(words1.size());
+        
+        for(int i = 0;i<words1.size();i++)
+        {
+            for(char c: words1[i])
+            {
+                freq[i][c]++;
+            }
+        }
+        
+        unordered_map<char,int> word2Freq;
+        for(int i = 0;i<words2.size();i++)
+        {
+            unordered_map<char,int> temp;
+            for(char c: words2[i])
+            {
+                temp[c]++;
+                if(word2Freq[c]<temp[c])
+                    word2Freq[c] = temp[c];
+            }
+        }
+        vector<string> result;
+        
+        for(int i = 0;i<words1.size();i++)
+        {
+            int flag = 0;
+            for(auto a : word2Freq)
+            {
+                char c = a.first;
+                int val = a.second;
+                if(freq[i][c]<val)
+                {
+                    flag = 1;
+                    break;
+                }   
+            }
+            if(flag==0)
+                result.push_back(words1[i]);
+        }
+        return result;
+        
+        
+    }
+};
