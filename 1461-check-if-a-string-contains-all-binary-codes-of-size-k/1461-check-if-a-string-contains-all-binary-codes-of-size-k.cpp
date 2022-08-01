a9913983c135ae4825fdffcccdabdddb50d22503
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-01 17:58:05 +0530

    Time: 489 ms (72.67%), Space: 58.8 MB (78.08%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,3 +1,39 @@
+/*
+ we can treat the string as a binary number, and take its decimal form as the hash value. In this case, each binary number has a unique hash value.
+*/
+class Solution 
+{
+public:
+    bool hasAllCodes(string s, int k) 
+    {
+        unordered_set<int> m;  
+        vector<int> roll(k,1);
+        
+        int p = 1000000007;
+        for(int i = k-2;i>=0;i--)
+            roll[i] = (roll[i+1]*2)%p;
+        
+        int hash = 0;
+        
+        for(int i = 0;i<s.size();i++)
+        {
+            hash = (hash*2 + s[i]-'0')%p;
+            
+            if(i>=k-1)
+            {
+                m.insert(hash);
+                hash = ((hash - (s[i-k+1]-'0')*roll[0])%p + p)%p;
+            }
+        }
+        if(m.size() == pow(2,k))
+            return true;
+        return false;
+        
+    }
+};
+
+/*
+
 class Solution 
 {
 public:
@@ -22,3 +58,4 @@ public:
         return false;
     }
 };
+*/
