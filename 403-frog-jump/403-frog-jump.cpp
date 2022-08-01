1a9e8fd34ac9babdd6b9f214e464271dcd90808a
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-02 00:54:55 +0530

    Time: 226 ms (62.35%), Space: 40.7 MB (58.86%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -8,7 +8,6 @@ public:
     {
         unordered_map<int,set<int>> reachedWithJumps;
         
-        
         for(int s : stones)
         {
             reachedWithJumps[s] = {};
@@ -18,29 +17,21 @@ public:
     
         for(int s : stones)
         {
-            if(s==0 and reachedWithJumps.find(1)!=reachedWithJumps.end())
+            for(int j : reachedWithJumps[s])
             {
-                reachedWithJumps[1].insert(1);
-            }
-            if(s!=0)
-            {
-                for(int j : reachedWithJumps[s])
+                if(reachedWithJumps.find(s+j+1)!=reachedWithJumps.end())
                 {
-                    if(reachedWithJumps.find(s+j+1)!=reachedWithJumps.end())
-                    {
-                        reachedWithJumps[s+j+1].insert(j+1);
-                    }
-                    if(reachedWithJumps.find(s+j)!=reachedWithJumps.end())
-                    {
-                        reachedWithJumps[s+j].insert(j);
-                    }
-                    if(j-1>0 and reachedWithJumps.find(s+j-1)!=reachedWithJumps.end())
-                    {
-                        reachedWithJumps[s+j-1].insert(j-1);
-                    }
+                    reachedWithJumps[s+j+1].insert(j+1);
+                }
+                if(reachedWithJumps.find(s+j)!=reachedWithJumps.end())
+                {
+                    reachedWithJumps[s+j].insert(j);
+                }
+                if(j-1>0 and reachedWithJumps.find(s+j-1)!=reachedWithJumps.end())
+                {
+                    reachedWithJumps[s+j-1].insert(j-1);
                 }
             }
-            
         }
     
         if(reachedWithJumps[stones[stones.size()-1]].size())
