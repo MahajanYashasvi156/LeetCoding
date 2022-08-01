868671fc40a28f769b3f90ea9462c85f8761f3fe
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-01 10:00:25 +0530

    Time: 588 ms (29.53%), Space: 109.1 MB (51.02%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,4 +1,13 @@
-class Solution {
+/*
+Same as Longest common substirng - O(m*n)
+
+
+
+Using Rolling hash + Binary search here is tedious task - O((M+N)*log(min(M,N))
+
+*/
+class Solution 
+{
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         vector<vector<int>>dp(nums1.size()+1,vector<int>(nums2.size()+1,0));
