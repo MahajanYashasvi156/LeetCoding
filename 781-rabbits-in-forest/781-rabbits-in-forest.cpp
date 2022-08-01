b08ecc7845ddc53e7c9ec31c8bd7f9886600c0bd
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-01 18:26:10 +0530

    Time: 3 ms (92.66%), Space: 8.8 MB (17.43%) - LeetHub

--- 781-rabbits-in-forest/781-rabbits-in-forest.cpp
+++ 781-rabbits-in-forest/781-rabbits-in-forest.cpp
@@ -1,3 +1,10 @@
+/*
+The basic thought is: if answers[i] == anwers[j], we take them as the same color as far as possible.
+What does the possible mean? Well, we can see a example:
+Condider a anwer sequence [2, 2, 2, 2]. According to the Pigeonhole Principle, we cannot take all of them as the same color since there are at most 3 pigeonhole and 4 pigeons.
+
+The solution is using a freq map to store the freq of each color. Each time a pigeonhole is full when reset corresponding element in color.
+*/
 class Solution 
 {
 public:
@@ -9,15 +16,10 @@ public:
         for(int a : answers)
         {
             freq[a]++;
-            if(freq[a]>a+1)
-            {
-                freq[a] = 1;
-            }
             if(freq[a]==1)
-            {
                 ans = ans+a+1;
-            }
-            
+            if(freq[a]==a+1)
+                freq[a] = 0;
         }
         
         return ans;
