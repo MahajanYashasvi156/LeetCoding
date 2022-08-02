204a265112807f46c9755a99c1c641dcd429d19a
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-02 10:01:42 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (25.02%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,3 +1,26 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) 
+    {
+        vector<int> dp(nums.size(),0);
+        dp[0] = nums[0];
+        for(int i = 1;i<nums.size();i++)
+        {
+            int pick = nums[i];
+            if(i>1)
+                pick+=dp[i-2];
+            int notPick = dp[i-1];
+            dp[i] = max(pick,notPick);
+        }
+        return dp[nums.size()-1];
+    }
+};
+
+
+/*
+
+*/
+/*
 class Solution 
 {
 public:
@@ -15,10 +38,10 @@ public:
             if(i==1)
                 pick[i] = nums[i];
             else
-                pick[i] = max(pick[i-2],Notpick[i-2])+nums[i];
-            
+                pick[i] = max(pick[i-2],Notpick[i-2])+nums[i];  
         }
         
         return max(pick[nums.size()-1],Notpick[nums.size()-1]);
     }
 };
+*/
