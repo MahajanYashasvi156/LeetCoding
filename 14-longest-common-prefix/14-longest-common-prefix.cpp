3cba61991e0c1606faa1a1652fb0f9086409c69a
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-03 01:25:00 +0530

    Time: 3 ms (92.59%), Space: 9.7 MB (11.25%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,4 +1,6 @@
-class Solution {
+/*
+class Solution 
+{
 public:
     string longestCommonPrefix(vector<string>& strs)
     {
@@ -14,5 +16,60 @@ public:
             prefix+=c;
         }
         return prefix;
+    }
+};
+*/
+
+class Node
+{
+    public:
+        Node* links[26];
+};
+class Trie
+{
+    public:
+    Node *node = new Node();
+    
+    void insert(string s)
+    {
+        Node *root = node;
+        
+        for(int i = 0;i<s.size();i++)
+        {
+            if(root->links[s[i]-'a']==NULL)
+                root->links[s[i]-'a'] = new Node();
+            root = root->links[s[i]-'a'];
+        }
+    }
+    
+    int searchPrefix(string s)
+    {
+        int size = 0;
+         Node *root = node;
+        for(int i = 0;i<s.size();i++)
+        {
+            if(root->links[s[i]-'a']==NULL)
+                return size;
+            root = root->links[s[i]-'a'];
+            size++;
+        }
+        return size;
+    }
+};
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& s) 
+    {
+        Trie trie;
+        trie.insert(s[0]);
+        
+        int size = s[0].size();
+        
+        for(int i = 1;i<s.size();i++)
+        {
+            size = min(size,trie.searchPrefix(s[i]));
+        }
+        
+        return s[0].substr(0,size);
     }
 };
