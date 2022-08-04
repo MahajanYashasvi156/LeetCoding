1bf067bf3fa4f9a2cde043e21384fc0505f9fcf9
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-04 19:18:43 +0530

    Time: 7 ms (9.72%), Space: 6.2 MB (49.21%) - LeetHub

---
+++ 343-integer-break/343-integer-break.cpp
@@ -0,0 +1,29 @@
+class Solution 
+{
+    
+public:
+    int integerBreak(int n) 
+    {
+        //num(0 to n-1) sum(0 to n)
+        vector<int>dp(n+1);
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 1;i<n;i++)
+        {
+            for(int j = 2;j<=n;j++)
+            {
+                if(j>=i)
+                {
+                    if(dp[j-i])
+                    {
+                        dp[j] = max(dp[j],i*dp[j-i]);
+                    }
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+};
