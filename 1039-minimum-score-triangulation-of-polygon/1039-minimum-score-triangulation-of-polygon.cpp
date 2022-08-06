4f42afb54377a15c7be4f92cd986a55973b05e99
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-07 00:04:16 +0530

    Time: 23 ms (13.63%), Space: 8.6 MB (46.62%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -1,3 +1,34 @@
+class Solution 
+{
+    int MCM(vector<int>& values,vector<vector<int>>&dp,int l,int r)
+    {
+        if(l>=r)
+            return 0;
+        
+        if(dp[l][r]!=-1)
+            return dp[l][r];
+        
+        dp[l][r] = INT_MAX;
+        for(int p = l;p<r;p++)
+        {
+            int currCost = MCM(values,dp,l,p) + MCM(values,dp,p+1,r) + values[l-1]*values[p]*values[r];
+            
+            dp[l][r] = min(dp[l][r],currCost);
+        }
+        return dp[l][r];
+    }
+public:
+    int minScoreTriangulation(vector<int>& values) 
+    {
+        int n = values.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        
+        return MCM(values,dp,1,n-1);
+        
+    }
+};
+
+
 /*
 Link - https://leetcode.com/problems/minimum-score-triangulation-of-polygon/
 TC - O(N^3)
@@ -9,7 +40,7 @@ We will take base of polygon and will form all possible triangles and this will 
 abcde, aec as triangle will give subproblems as abc cde whose values are already calculated
 so ans for abcde = ans for abc + ans for cde + a*e*c
 */
-
+/*
 class Solution 
 {
 public:
@@ -35,3 +66,4 @@ public:
         return dp[0][n-1];
     }
 };
+*/
