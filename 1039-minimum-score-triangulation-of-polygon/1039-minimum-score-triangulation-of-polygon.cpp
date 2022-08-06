c16d6ad206e9e2e77de3181dd6b8a28c595320df
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-07 00:15:52 +0530

    Time: 9 ms (75.68%), Space: 8.6 MB (46.62%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -1,3 +1,14 @@
+/*
+Link - https://leetcode.com/problems/minimum-score-triangulation-of-polygon/
+TC - O(N^3)
+SC - O(N^2)
+
+Gap strategy on DP same as MCM Baloon Birst
+
+We will take base of polygon and will form all possible triangles and this will give us subproblems 
+abcde, aec as triangle will give subproblems as abc cde whose values are already calculated
+so ans for abcde = ans for abc + ans for cde + a*e*c
+*/
 class Solution 
 {
 public:
@@ -54,42 +65,4 @@ public:
     }
 };
 
-*/
-/*
-Link - https://leetcode.com/problems/minimum-score-triangulation-of-polygon/
-TC - O(N^3)
-SC - O(N^2)
-
-Gap strategy on DP same as MCM Baloon Birst
-
-We will take base of polygon and will form all possible triangles and this will give us subproblems 
-abcde, aec as triangle will give subproblems as abc cde whose values are already calculated
-so ans for abcde = ans for abc + ans for cde + a*e*c
-*/
-/*
-class Solution 
-{
-public:
-    int minScoreTriangulation(vector<int>& values) 
-    {
-        int n = values.size();
-        vector<vector<int>>dp(n,vector<int>(n,0));
-        
-        for(int l = 2;l<n;l++)
-        {
-            for(int i=0;i<n-l;i++)
-            {
-                int j = l+i;
-                dp[i][j] = INT_MAX;
-                
-                for(int k = i+1; k<j;k++)
-                {
-                    dp[i][j]=min(dp[i][j],dp[i][k]+dp[k][j]+ values[i]*values[k]*values[j]);
-                }
-            }
-            
-        }
-        return dp[0][n-1];
-    }
-};
 */
