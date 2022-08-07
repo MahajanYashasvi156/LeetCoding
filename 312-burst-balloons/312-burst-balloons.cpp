5a9ceb41b9b78c9add39d0d586e810e1b4865a01
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-07 20:48:30 +0530

    Time: 959 ms (20.74%), Space: 10.3 MB (52.71%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -1,6 +1,70 @@
 class Solution 
 {
 public:
+    int MCM(vector<int>&nums,vector<vector<int>>&dp,int l,int r)
+    {
+        if(l>=r)
+            return 0;
+        
+        if(dp[l][r]!=-1)
+            return dp[l][r];
+        
+        dp[l][r] = INT_MIN;
+        for(int p = l;p<r;p++)
+        {
+            int currCoins = MCM(nums,dp,l,p)+  MCM(nums,dp,p+1,r) + nums[l-1]*nums[p]*nums[r];
+            
+            dp[l][r] = max(dp[l][r],currCoins);
+        }
+        return dp[l][r];
+    }
+    int maxCoins(vector<int>& nums) 
+    {
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);
+        vector<vector<int>> dp(nums.size(),vector<int>(nums.size(),-1));
+        
+        return MCM(nums,dp,1,nums.size()-1);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Link - https://leetcode.com/problems/burst-balloons/submissions/
+
+TC - O(N^3)
+SC - O(N^2)
+
+Application of MCM.
+
+Each cell represents maximum coins collected bursting that ballons(considering them to be in the nums array itself.)
+
+
+class Solution 
+{
+public:
     int maxCoins(vector<int>& nums) 
     {
         int n = nums.size();
@@ -42,3 +106,4 @@ public:
         return dp[0][n-1];
     }
 };
+*/
