dd6396ed06a17eaaa68758a8281731c3cc10684c
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-08 13:34:37 +0530

    Time: 705 ms (21.82%), Space: 6.4 MB (97.25%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,4 +1,3 @@
-/*
 class Solution 
 {
     bool isPalindrome(string &s,int l,int h)
@@ -16,36 +15,32 @@ public:
     int minCut(string s) 
     {
         int n = s.size();
-        vector<vector<int>> dp(n,vector<int>(n,0));
+        vector<int> dp(n,0);
         
-        for(int len = 2;len<=n;len++)
+        int j = n-1;
+        
+        for(int i = n-1;i>=0;i--)
         {
-            for(int i = 0;i<=n-len;i++)
+            int ans = INT_MAX;
+            for(int k = j;k>=i;k--)
             {
-                int j = i+len-1;
-                 cout<<i<<" "<<j<<endl;
-                int ans = INT_MAX;
-                for(int k = i;k<=j;k++)
+                if(isPalindrome(s,i,k))
                 {
-                    if(isPalindrome(s,i,k))
-                    {
-                        cout<<i<<" "<<k<<endl;
-                        if(k<j)
-                            ans = min(ans, 1 + dp[k+1][j]);
-
-                    }
+                    if(k<j)
+                        ans = min(ans,1+dp[k+1]);
+                    else
+                        ans = 0;
                 }
-                
-                dp[i][j] = ans;
-                cout<<dp[i][j]<<endl;
             }
+
+            dp[i] = ans;
         }
-        return dp[0][n-1];
+        return dp[0];
     }
 };
 
-*/
 
+/*
 class Solution 
 {
     bool isPalindrome(string &s,int l,int h)
