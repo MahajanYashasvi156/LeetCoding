b4d31d9d88c3176aa8b31d371791e2dba271c920
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-08 13:07:35 +0530

    Time: 532 ms (43.88%), Space: 6.7 MB (72.14%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution 
 {
     bool isPalindrome(string &s,int l,int h)
@@ -11,13 +12,62 @@ class Solution
         }
         return true;
     }
-    int MCM(string &s,vector<vector<int>> &dp,int i,int j)
+public:
+    int minCut(string s) 
     {
+        int n = s.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        
+        for(int len = 2;len<=n;len++)
+        {
+            for(int i = 0;i<=n-len;i++)
+            {
+                int j = i+len-1;
+                 cout<<i<<" "<<j<<endl;
+                int ans = INT_MAX;
+                for(int k = i;k<=j;k++)
+                {
+                    if(isPalindrome(s,i,k))
+                    {
+                        cout<<i<<" "<<k<<endl;
+                        if(k<j)
+                            ans = min(ans, 1 + dp[k+1][j]);
+
+                    }
+                }
+                
+                dp[i][j] = ans;
+                cout<<dp[i][j]<<endl;
+            }
+        }
+        return dp[0][n-1];
+    }
+};
+
+*/
+
+class Solution 
+{
+    bool isPalindrome(string &s,int l,int h)
+    {
+        while(l<h)
+        {
+            if(s[l]!=s[h])
+                return false;
+            l++;
+            h--;
+        }
+        return true;
+    }
+    int MCM(string &s,vector<int> &dp,int i)
+    {
+        int j = s.size()-1;
+        
         if(i>=j)
             return 0;
         
-        if(dp[i][j]!=-1)
-            return dp[i][j];
+        if(dp[i]!=-1)
+            return dp[i];
         
         int ans = INT_MAX;
         
@@ -26,22 +76,20 @@ class Solution
             if(isPalindrome(s,i,k))
             {
                 if(k==j)
-                    ans = min(ans,MCM(s,dp,k+1,j));
+                    ans = min(ans,MCM(s,dp,k+1));
                 else
-                    ans = min(ans, 1 +  MCM(s,dp,k+1,j));
+                    ans = min(ans, 1 + MCM(s,dp,k+1));
             }
         }
-        dp[i][j]=ans;
-        return ans;
+        
+        return dp[i]=ans;
     }
 public:
     int minCut(string s) 
     {
         int n = s.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        
-        return MCM(s,dp,0,n-1);
-        
+        vector<int> dp(n,-1);
+        return MCM(s,dp,0);
     }
 };
 
