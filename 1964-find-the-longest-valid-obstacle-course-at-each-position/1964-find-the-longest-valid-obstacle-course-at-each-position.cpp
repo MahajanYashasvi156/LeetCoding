b63ab58fcc96e78758945799f504ce457d61c714
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-09 11:54:34 +0530

    Time: 354 ms (80.77%), Space: 123.1 MB (41.61%) - LeetHub

--- 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
+++ 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) 
     {
-        vector<int> res;
+        vector<int> LIS;
         vector<int> ans;
         
         for(int i = 0;i<obstacles.size();i++)
         {
-            auto it = upper_bound(res.begin(),res.end(),obstacles[i]) - res.begin();
+            int index = upper_bound(LIS.begin(),LIS.end(),obstacles[i]) - LIS.begin();
             
-            if(it==res.size())
-                res.push_back(obstacles[i]);
+            if(index==LIS.size())
+                LIS.push_back(obstacles[i]);
             else
-                res[it] = obstacles[i];
-            ans.push_back(it+1);
+                LIS[index] = obstacles[i];
+            ans.push_back(index+1);
         }
         return ans;
     }
