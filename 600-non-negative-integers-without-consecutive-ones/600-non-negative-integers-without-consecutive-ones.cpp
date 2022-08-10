d5dcc2e3c283b78263b468bae4ea9830f138d556
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-10 19:05:35 +0530

    Time: 68 ms (5.00%), Space: 14.2 MB (5.50%) - LeetHub

---
+++ 600-non-negative-integers-without-consecutive-ones/600-non-negative-integers-without-consecutive-ones.cpp
@@ -0,0 +1,41 @@
+class Solution 
+{
+    int solve(string s, int index, int prevbit,int tight,vector<vector<vector<int>>>&dp)
+    {
+        if(index==s.size())
+            return 1;
+        
+        if(dp[index][prevbit][tight]!=-1)
+           return dp[index][prevbit][tight];
+        
+        int ub = tight ? (s[index]-'0') : 1;
+        
+        int ans = 0;
+        
+        for(int bit = 0;bit<=ub;bit++)
+        {
+            if(bit==0)
+                ans = ans + solve(s,index+1,0,tight and bit==ub,dp);
+            
+            if(prevbit==0 and bit==1)
+            {
+                ans = ans + solve(s,index+1,1,tight and (bit==ub),dp);
+            }
+        }
+        return dp[index][prevbit][tight] = ans;
+    }
+public:
+    int findIntegers(int n) 
+    {
+        string s ="";
+        while(n)
+        {
+            s = to_string(n%2) + s;
+            n = n/2;
+        }
+     
+        vector<vector<vector<int>>> dp(s.size(),vector<vector<int>>(2,vector<int>(2,-1)));
+        
+        return solve(s,0,0,1,dp);
+    }
+};
