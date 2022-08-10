5762568bc00c736dbe40385bdb8b8079a5f7a2a1
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-10 23:40:40 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (27.06%) - LeetHub

--- 902-numbers-at-most-n-given-digit-set/902-numbers-at-most-n-given-digit-set.cpp
+++ 902-numbers-at-most-n-given-digit-set/902-numbers-at-most-n-given-digit-set.cpp
@@ -12,12 +12,13 @@ class Solution
         
         int ans = 0;
         
+        //If the number is not started till index , we have option to delay the formation of number by putting leading 0.
         if(hasStarted==0)
             ans +=solve(s,index+1,0,tight and ub== 0, digits,dp);
+        
         for(int i = 0;i<digits.size() and stoi(digits[i])<=ub;i++)
-        {
             ans +=solve(s,index+1,1,tight and ub==stoi(digits[i]) , digits,dp);         
-        }
+    
         return dp[index][hasStarted][tight] = ans;
     }
 public:
@@ -25,8 +26,12 @@ public:
     {
         string s = to_string(n);
         
+        //Three States - Index , HasStarted, tight 
+        //hasStarted - whether the number formation has started or not.
+        //If the constraints is tight or not. 
         vector<vector<vector<int>>> dp(s.size(),vector<vector<int>>(2,vector<int>(2,-1)));
         
+        //By delaying the formation of number. We will form 0 also which can't be possible with positive non zero number.
         return solve(s,0,0,1,digits,dp) - 1;
     }
 };
