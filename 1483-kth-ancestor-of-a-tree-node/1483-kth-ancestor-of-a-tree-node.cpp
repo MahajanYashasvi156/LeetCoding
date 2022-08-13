44253137c3e5d233441c38e6fb9c50adce7d3318
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-13 19:24:43 +0530

    Time: 603 ms (36.75%), Space: 252.9 MB (5.08%) - LeetHub

--- 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
+++ 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
@@ -1,8 +1,7 @@
 class TreeAncestor 
-{
-   
+{ 
 public:
-     vector<vector<int>> up{50000,vector<int>(16,-1)};
+    vector<vector<int>> up{50000,vector<int>(16,-1)};
     vector<vector<int>> child{50000};
     
     void binary_lifting(int src,int par)
@@ -12,26 +11,19 @@ public:
         for(int i = 1;i<16;i++)
         {
             if(up[src][i-1]!=-1)
-            {
                 up[src][i] = up[up[src][i-1]][i-1];
-            }
+        
             else
-            {
                 up[src][i] = -1;
-            }
         }
         
         for(int c : child[src])
-        {
             binary_lifting(c,src);
-        }
     }
     TreeAncestor(int n, vector<int>& parent) 
     {
         for(int i = 1;i<n;i++)
-        {
             child[parent[i]].push_back(i);
-        }
         
         binary_lifting(0,-1);
     }
