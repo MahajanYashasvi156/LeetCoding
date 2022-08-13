c605ed4e9acf6bb30830056c36566c368947b0c4
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-13 19:29:09 +0530

    Time: 738 ms (11.17%), Space: 252.9 MB (5.08%) - LeetHub

--- 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
+++ 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
@@ -6,25 +6,29 @@ public:
     
     void binary_lifting(int src,int par)
     {
+        //Node which is 2^0 level = 1 level up from curretn node is the parent node.
         up[src][0] = par;
         
         for(int i = 1;i<16;i++)
         {
+            //if 2^i-1 level up of current node has beyond root then 2^i will also be beyond root.
             if(up[src][i-1]!=-1)
                 up[src][i] = up[up[src][i-1]][i-1];
-        
+    
             else
                 up[src][i] = -1;
         }
         
+        //Parents all ancestors are evaluated now we can call to child. Child will use the information of parent to evaluate their ancestors.
         for(int c : child[src])
             binary_lifting(c,src);
     }
     TreeAncestor(int n, vector<int>& parent) 
     {
+        //Building a graph to access child of current node.
         for(int i = 1;i<n;i++)
             child[parent[i]].push_back(i);
-        
+    
         binary_lifting(0,-1);
     }
     
@@ -32,8 +36,10 @@ public:
     {
         if(node==-1 || k ==0)
             return node;
+        
         for(int i = 16;i>=0;i--)
         {
+            //each time find the largest jump we can make and from that point calcuated our expected ancestor.
             if(k & (1<<i))
                 return getKthAncestor(up[node][i],k-(1<<i));
         }
