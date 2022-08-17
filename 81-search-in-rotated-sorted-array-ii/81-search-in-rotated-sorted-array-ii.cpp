f63444a8fbddc03e26d36528c95bd8714c2de0c0
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-17 18:19:40 +0530

    Time: 7 ms (82.09%), Space: 13.9 MB (93.45%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,3 +1,19 @@
+/*
+     
+     Note :- Just identify which part of array is sorted and based on that make decision.
+     
+     1. If the left part is sorted (nums[mid]>nums[low]) then
+            check whether our target can exist in that sorted left part based on range of sorted array.
+            If yes then high = mid-1;
+            else go to right part , low = mid+1
+    
+    2. If the right part is sorted
+        if the target lies within the range of right sorted array then go to right else left subarray.
+        
+        For handling duplicates, increase the low pointer whenever low element and middle element is equal
+            
+
+*/
 class Solution {
 public:
     bool search(vector<int>& nums, int target)
