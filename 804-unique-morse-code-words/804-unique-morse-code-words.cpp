fbc8a66ad811a979ed55a2965444d96d5f87cbf8
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-17 12:21:50 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (98.15%) - LeetHub

---
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) 
+    {
+        string table[26] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        unordered_map<string,int>m;
+        
+        for(string s : words)
+        {
+            string transformation = "";
+            for(char c: s)
+            {
+                transformation = transformation + table[c-'a'];
+            }
+            
+            m[transformation]++;
+        }
+        return m.size();
+    }
+};
