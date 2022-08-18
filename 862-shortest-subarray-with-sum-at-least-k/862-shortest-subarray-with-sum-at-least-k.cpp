2b47731d99de8001d4e28f5a94d0c6edcdb990ad
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-18 14:24:55 +0530

    Time: 312 ms (79.06%), Space: 110 MB (40.03%) - LeetHub

--- 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.cpp
+++ 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,4 +1,24 @@
-class Solution {
+/*
+    Two Pointer Algorithm If not have positive values.
+    
+    1. left = 0,right = 0;
+    2. Increase right till ws>=target. Update shortest= right+1
+    3. Now compress the window by increasing left till we get the ws<target.
+        update shortest including elements from left-1 to right.shortest=right-left+2
+        
+    But here because of the presence of negative values we will not get monotonically increasing sum. So for monotonically increasing sum we could think of maintianing stack.
+    But we also want to compress window by popping sum from the front so we can maintain queue here.
+    
+    1. make a pair of queue , sum and index
+    2. If the sum >=k then we can find the minimum window size shortest = i+1.
+    3. Pop_front elements till we can get sum>=k
+       the last element of deque we popped has the start of window. 
+       shortest = min(shortest,i-curr.second);
+    4. For maintaining montonically sum , we pop elements of deque from last till we get the first value<currsum.
+    5. Then we can safely add the curr sum to the deque maintaining the monotonic nature.
+*/
+class Solution 
+{
 public:
     int shortestSubarray(vector<int>& nums, int k)
     {
