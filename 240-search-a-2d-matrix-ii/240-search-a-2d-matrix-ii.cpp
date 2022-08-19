4d37e00761a357045d56c700713f3648ed825d2c
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-19 15:44:50 +0530

    Time: 249 ms (30.23%), Space: 15 MB (21.60%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -3,6 +3,30 @@ class Solution
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) 
     {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        int i = 0;
+        int j = n-1;
+        while(i<m and j>=0)
+        {
+            if(matrix[i][j] == target)
+                return true;
+            if(matrix[i][j]<target)
+                i++;
+            else
+                j--;
+        }
+        return false;
+    }
+};
+
+/*
+class Solution 
+{
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
         
         for(int i = 0;i<matrix.size();i++)
         {
@@ -14,3 +38,4 @@ public:
         return false;
     }
 };
+*/
