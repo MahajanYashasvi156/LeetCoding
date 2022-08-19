80ef88f895057d4d97a4c33c708598687816f435
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-19 15:39:41 +0530

    Time: 628 ms (7.28%), Space: 14.7 MB (97.76%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        
+        for(int i = 0;i<matrix.size();i++)
+        {
+            auto it = lower_bound(matrix[i].begin(),matrix[i].end(),target);
+            
+            if(it!=matrix[i].end() and *it == target)
+                return true;
+        }
+        return false;
+    }
+};
