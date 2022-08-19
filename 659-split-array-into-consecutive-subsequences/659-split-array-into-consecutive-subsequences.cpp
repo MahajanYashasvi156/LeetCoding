82175adf2effed813e773a062a2113c864475294
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-19 15:17:13 +0530

    Time: 498 ms (5.07%), Space: 73.9 MB (9.80%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -1,4 +1,17 @@
-class Solution {
+/*
+    Algorithm - 
+    
+    1. Make a map - key value - element,index
+    
+    2. Make a count Array to store the max possible length till current.
+    
+    3. Check nums[i] -1 exist in map if yes then pick the one with the minimum count value. Set its count value = -1 and remove that previous elemnt from the map also.
+    
+    4. Check if all elements should have count values either -1 or >=3
+
+*/
+class Solution 
+{
 public:
     bool isPossible(vector<int>& nums)
     {
