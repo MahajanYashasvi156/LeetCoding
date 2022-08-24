42e8137c3b6e55d093b131e01c829ba30a860605
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-24 19:56:46 +0530

    Time: 4 ms (40.16%), Space: 5.9 MB (71.30%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -4,7 +4,7 @@ public:
     {
         for(int i = 0;i<32;i=i+2)
         {
-            if(n==(1<<i))
+            if((n ^ (1<<i)) ==0)
                 return true;
         }
         return false;
