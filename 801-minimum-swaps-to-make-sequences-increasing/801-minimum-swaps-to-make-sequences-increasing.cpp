ba3926874f1e04eb64ab5d0187603536765e0707
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-08-28 23:38:48 +0530

    Time: 297 ms (74.65%), Space: 94.2 MB (72.48%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -13,22 +13,16 @@ public:
         for(int i = 1;i<n;i++)
         {
             if(nums1[i]>nums1[i-1] and nums2[i]>nums2[i-1])
-            {
                 noSwap[i] = min(noSwap[i],noSwap[i-1]);
-            }
+            
             if(nums1[i]>nums2[i-1] and nums2[i]>nums1[i-1])
-            {
                 noSwap[i] = min(noSwap[i],swap[i-1]);
-            }
             
             if(nums2[i]>nums1[i-1] and nums1[i]>nums2[i-1])
-            {
                 swap[i] = min(swap[i],noSwap[i-1]+1);
-            }
+            
             if(nums2[i]>nums2[i-1] and nums1[i]>nums1[i-1])
-            {
                 swap[i] = min(swap[i],swap[i-1]+1);
-            }
         }
         return min(swap[n-1],noSwap[n-1]);
     }
