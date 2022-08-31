76947a6b123f78785e834a736424a213cc3eb309
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-09-01 00:35:17 +0530

    Time: 31 ms (74.92%), Space: 11.1 MB (54.80%) - LeetHub

--- 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
@@ -1,4 +1,10 @@
-class Solution {
+/*
+
+https://leetcode.com/problems/count-unique-characters-of-all-substrings-of-a-given-string/
+
+*/
+class Solution 
+{
 public:
     long long countVowels(string word) 
     {
