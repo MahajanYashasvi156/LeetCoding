bf2c6f344691f20cbbd0012bfae0478f553edaa1
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-09-01 20:16:23 +0530

    Time: 1184 ms (5.03%), Space: 89.8 MB (28.22%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -8,6 +8,19 @@ class Solution
 public:
     int findMinArrowShots(vector<vector<int>>& points) 
     {
+        /*
+        
+            TC - [[9,12],[1,10],[4,11],[8,12],[3,9],[6,9],[6,7]]
+            
+            Logic :- We can shot the ballons in once arrow if that will have atleast one point in common.
+            
+            Sort on the basis of end of the ballon. If there exist any ballon whose end point is after current ballon and its start points is before the end of current point. then they will surely have an overlapping case.
+              ___
+            _______
+        ________________       
+        
+        All three can be shot in one arrow only.
+        */
         sort(points.begin(),points.end(),cmp);
         
         int ans = 0;
