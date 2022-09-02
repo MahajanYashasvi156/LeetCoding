c3f1733113d198967222fc45ed1ee27a128d986b
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-09-03 01:11:36 +0530

    Time: 3 ms (46.86%), Space: 7.2 MB (30.11%) - LeetHub

--- 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.cpp
+++ 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.cpp
@@ -1,4 +1,11 @@
-class Solution {
+/*
+    Intution - Pick the element which is the largest till now i.e. maxi. Now the correct possition of this maxi is at index = maxi. So we the chunk size should be atleast from the original incorrect position of maxi to the correct position of maxi. 
+    
+    If in between some other elemnt greatr than maxi appears then we have to expand our chunk size so the current maxi will be at its correct position.
+    
+*/
+class Solution 
+{
 public:
     int maxChunksToSorted(vector<int>& arr)
     {
