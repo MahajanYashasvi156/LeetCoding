ef75fbd67cdd1557480b830aea765f9562de352c
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-09-02 15:35:18 +0530

    Time: 28 ms (42.21%), Space: 9.4 MB (27.32%) - LeetHub

---
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+     int MCM(vector<int>&arr,vector<vector<int>>&dp,int l,int r,vector<vector<int>>&maxVal)
+     {
+         if(l>=r)
+             return 0;
+         
+         if(dp[l][r]!=-1)
+             return dp[l][r];
+         
+         long ans = INT_MAX;
+         for(int p = l;p<r;p++)
+         {
+            long currSum = maxVal[l][p]*maxVal[p+1][r]+MCM(arr,dp,l,p,maxVal) +MCM(arr,dp,p+1,r,maxVal);
+            ans = min(ans,currSum);
+         }
+         
+         return dp[l][r] = ans;
+     }
+    int mctFromLeafValues(vector<int>& arr) 
+    {
+        vector<vector<int>> maxVal(arr.size(),vector<int>(arr.size(),INT_MIN));
+        vector<vector<int>> dp(arr.size(),vector<int>(arr.size(),-1));
+        
+        for(int i = 0;i<arr.size();i++)
+        {
+            int maxi = INT_MIN;
+            
+            for(int j = i;j<arr.size();j++)
+            {
+                maxi = max(maxi,arr[j]);
+                maxVal[i][j] = maxi;
+               // cout<<i<<" "<<j<<" "<<maxVal[i][j]<<endl;
+            }
+            
+            
+        }
+        
+        return MCM(arr,dp,0,arr.size()-1,maxVal);
+    }
+};
