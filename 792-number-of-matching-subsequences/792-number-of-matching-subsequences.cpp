5a7911115c48bd5fab9f97c06a4fee9d32dd3ad4
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-09-03 19:49:53 +0530

    Time: 557 ms (26.91%), Space: 324.1 MB (16.26%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,9 +1,81 @@
-class Solution {
+class Node
+{
+    public:
+    
+    Node* childrens[26]={NULL};
+    int end = 0;
+};
+
+class Trie
+{ 
+    public:
+    Node* root  = new Node();
+        void insert(string s)
+        {
+            Node* curr = root;
+            
+            for(char c : s)
+            {
+                if(curr->childrens[c-'a']==NULL)
+                {
+                    curr->childrens[c-'a'] = new Node();
+                   
+                }
+                 curr = curr->childrens[c-'a'];
+            }
+            curr->end++;
+        }
+    
+        int dfs(Node* root,string &s,vector<vector<int>>&pos,int index)
+        {
+            int result = 0;
+            
+            for(int i = 0;i<26;i++)
+            {
+                if(root->childrens[i])
+                {
+                    int newIndex = upper_bound(pos[i].begin(),pos[i].end(),index) - pos[i].begin();
+                    
+                    if(newIndex==pos[i].size())
+                        continue;
+                    result = result + dfs(root->childrens[i],s,pos,pos[i][newIndex]);
+                }
+            }
+            return result + root->end;
+        }
+};
+class Solution 
+{
+public:
+    int numMatchingSubseq(string s, vector<string>& words) 
+    {
+        Trie trie;
+        
+        for(string str:words)
+            trie.insert(str);
+        
+        vector<vector<int>> pos(26);
+        
+        for(int i = 0;i<s.size();i++)
+        {
+            pos[s[i]-'a'].push_back(i);
+        }
+        
+        return trie.dfs(trie.root,s,pos,-1);
+    }
+};
+
+
+
+/*
+class Solution 
+{
 public:
     int numMatchingSubseq(string s, vector<string>& words) 
     {
         unordered_map<char,vector<string>> m;
         
+        //Which words are waiting for which words is stored here.
         for(string str: words)
             m[str[0]].push_back(str);
         
@@ -27,3 +99,4 @@ public:
         return m['$'].size();
     }
 };
+*/
