b65ceab7784aa07fac8dc57c5a25cfa9b366f061
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-09-03 22:52:41 +0530

    Time: 79 ms (6.30%), Space: 31 MB (20.96%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -9,25 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    int countNodes(TreeNode* root) 
-    {
-        if(root==NULL)
-            return 0;
-        
-        TreeNode* l = root;
-        int lh = 0;
-        
-        while(l) 
-            lh++,l = l->left;
-        TreeNode* r = root;
-        int rh = 0;
-        while(r)
-            rh++,r = r->right;
-        
-        if(lh==rh)
-            return pow(2,lh)-1;
-        return 1 + countNodes(root->left)+countNodes(root->right);
-    }
+class Solution 
+{
+    public:
+        int countNodes(TreeNode* root) 
+        {
+            if(root==NULL)
+                return 0;
+
+            TreeNode* l = root;
+            int lh = 0;
+
+            while(l) 
+                lh++,l = l->left;
+
+            TreeNode* r = root;
+            int rh = 0;
+            while(r)
+                rh++,r = r->right;
+
+            if(lh==rh)
+                return pow(2,lh)-1;
+            return 1 + countNodes(root->left)+countNodes(root->right);
+        }
 };
