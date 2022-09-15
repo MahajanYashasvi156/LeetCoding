6582644d5d4f4cc8abf25d5f8872bf8903aa0f19
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-09-16 00:53:19 +0530

    Time: 41 ms (24.49%), Space: 22.3 MB (77.04%) - LeetHub

--- 337-house-robber-iii/337-house-robber-iii.cpp
+++ 337-house-robber-iii/337-house-robber-iii.cpp
@@ -15,12 +15,15 @@ class Solution
     {
         if(root==NULL)
             return {0,0};
-        
-       pair<int,int> leftMoney = solve(root->left);
-       pair<int,int> rightMoney = solve(root->right);
-        
-        return {root->val+leftMoney.second+rightMoney.second , max(leftMoney.first,leftMoney.second)+max(rightMoney.first,rightMoney.second)};
-        
+
+        pair<int,int> leftMoney = solve(root->left);
+        pair<int,int> rightMoney = solve(root->right);
+
+        //If we are picking current node then the maximum we can get is by not taking our direct childs
+        int pick = root->val+leftMoney.second+rightMoney.second ;
+        //If not picking current node then we can take maximum of picking or notpicking the direct childrens.
+        int notPick = max(leftMoney.first,leftMoney.second)+max(rightMoney.first,rightMoney.second);
+        return {pick,notPick};
     }
 public:
     int rob(TreeNode* root) 
