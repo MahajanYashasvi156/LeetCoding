e42eed0d259c2906037073dfe05c138cbec91b2b
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-09-17 15:58:48 +0530

    Time: 335 ms (49.66%), Space: 93.8 MB (47.66%) - LeetHub

--- 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -1,4 +1,4 @@
-class Solution 
+/*class Solution 
 {
     int M = 1000000007;
     int solve(int startPos , int endPos, unordered_map<int,unordered_map<int,int>> &dp,int k)
@@ -19,5 +19,33 @@ public:
         unordered_map<int,unordered_map<int,int>> dp;
         
         return solve(startPos,endPos,dp,k);
+    }
+};
+*/
+
+class Solution 
+{
+    int M = 1000000007;
+    int solve(int startPos , int endPos,vector<vector<int>> &dp
+,int k)
+    {
+        if(k==0)
+            return startPos==endPos;
+        
+        if(dp[startPos][k]!=-1)
+            return dp[startPos][k];
+        int forward = solve(startPos+1,endPos,dp,k-1);
+        int backward = solve(startPos-1,endPos,dp,k-1);
+        
+        return dp[startPos][k] = (forward + backward)%M;
+    }
+public:
+    int numberOfWays(int startPos, int endPos, int k) 
+    {
+        int newStartPos = k;
+
+        int newEndPos = newStartPos + (endPos-startPos);
+        vector<vector<int>> dp(2*k+1,vector<int>(k+1,-1));
+        return solve(newStartPos,newEndPos,dp,k);
     }
 };
