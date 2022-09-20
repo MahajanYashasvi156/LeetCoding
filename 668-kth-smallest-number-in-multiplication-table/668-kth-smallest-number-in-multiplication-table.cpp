209c028b6203be5b983194ad8b35a143473aa755
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-09-20 21:13:55 +0530

    Time: 22 ms (48.58%), Space: 5.9 MB (28.27%) - LeetHub

--- 668-kth-smallest-number-in-multiplication-table/668-kth-smallest-number-in-multiplication-table.cpp
+++ 668-kth-smallest-number-in-multiplication-table/668-kth-smallest-number-in-multiplication-table.cpp
@@ -23,7 +23,8 @@ public:
             mid = low + (high-low)/2;
             int count = findLessEqualCount(mid,m,n);
             
-            //if this count is = to k means we found the ans, but if this >k it may also possible that mid will be the ans when there are repeatations of mid element.
+            //if this >k it may also possible that mid will be the ans when there are repeatations of mid element. But if ==k then also it may possible that mid doesn't present in our multiplication table. In that case let's say mid is 5 in 2*3 muliplication table and we want 5th smallest value. In this case the count we get is 5 i.e. 1,2,3 and 2,4 . Clearly but the problem is 5 doesn't exist in the table. So we have to check just smaller value than 5 that exist in table.
+            
             if(count>=k)
             {
                 ans = mid;
