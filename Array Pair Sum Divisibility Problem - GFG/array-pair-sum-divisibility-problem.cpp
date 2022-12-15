b5927e621e327643e5c641da9492d1db323ccfef
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-12-15 21:47:29 +0530

    Added solution - LeetHub

--- Array Pair Sum Divisibility Problem - GFG/array-pair-sum-divisibility-problem.cpp
+++ Array Pair Sum Divisibility Problem - GFG/array-pair-sum-divisibility-problem.cpp
@@ -15,6 +15,29 @@ class Solution
         {
             int mod = n%k;
             
+            /*
+                4 2
+                
+                6 14 12 14
+                
+                for 6 -> mod = 0
+                for 14 -> mod = 0
+                for 12 -> mod = 0
+                for 14 -> mod = 0
+                
+                if already divisible by 2 then we have to find the pair which is also divisible by 2
+                so we need to take modulo.
+                
+                0 1 2 3 4 5 ______ k-1
+                
+                for 0 find a pair having mod 0
+                
+                for 1 find a pair having mod k-1
+                
+                for 2 find a pair having mod k-2
+                
+            
+            */
             if(m[(k-mod)%k]>0)
             {
                 m[(k-mod)%k]--;
