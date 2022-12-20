9869f55b03166dc18298105bacc08cb93b61bc5a
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-12-20 21:13:38 +0530

    Added solution - LeetHub

---
+++ A difference of values and indexes - GFG/a-difference-of-values-and-indexes.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    int maxDistance(int arr[], int n) 
+    { 
+       int min1 = INT_MAX;
+       int max1 = INT_MIN;
+       int min2 = INT_MAX;
+       int max2 = INT_MIN;
+       
+       for(int i = 0;i<n;i++)
+       {
+           min1 = min(min1,arr[i]-i);
+           max1 = max(max1,arr[i]-i);
+           
+           
+           min2 = min(min2,arr[i]+i);
+           max2 = max(max2,arr[i]+i);
+           
+       }
+       
+       return max(max1-min1,max2-min2);
+    } 
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+	int T;
+	cin>>T;
+	while(T--){
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i = 0; i<n; i++)
+	        cin>>arr[i];
+	    Solution obj;
+	    cout<<obj.maxDistance(arr, n)<<endl;
+	}
+} 
+
+// } Driver Code Ends
