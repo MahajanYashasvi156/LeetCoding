610d6edcfa8a06f5c85dba3fdfb3926ee67579f3
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2022-12-29 23:27:48 +0530

    Time: 1035 ms (70.10%), Space: 153 MB (23.01%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) 
+    {
+        for(int i = 0;i<tasks.size();i++)
+        {
+            tasks[i].push_back(i);
+        }
+        
+        sort(tasks.begin(),tasks.end());
+        
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> minHeap;
+        
+        int i = 0;
+        
+        int last = 0;
+        vector<int> ans;
+        
+        while(i<tasks.size())
+        {
+            if(minHeap.empty()==false)
+            {
+                last = last + minHeap.top()[0];
+                ans.push_back(minHeap.top()[1]);
+                minHeap.pop();
+            }
+            while(i<tasks.size() and tasks[i][0]<=last)
+            {
+                 minHeap.push({tasks[i][1],tasks[i][2]});
+                i++;
+            }
+            if(i<tasks.size() and last<tasks[i][0] and minHeap.empty())
+            {
+                last = tasks[i][0];
+            } 
+        }
+        while(minHeap.empty()==false)
+        {
+            ans.push_back(minHeap.top()[1]);
+            minHeap.pop();
+        }
+        return ans;
+    }
+};
