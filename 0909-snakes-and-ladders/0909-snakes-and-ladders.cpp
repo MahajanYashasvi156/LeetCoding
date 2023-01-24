a344bb33d41b871e2c002317e8bd752c6eed966b
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2023-01-25 02:38:16 +0530

    Time: 14 ms (94.94%), Space: 12.7 MB (78.06%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -17,7 +17,7 @@ public:
             {
                 int curr = q.front();
                 q.pop();
-                //cout<<curr<<" ";
+               
                 if(curr==n*n)
                     return step;
                 
@@ -26,9 +26,9 @@ public:
                     int r = n-1-(i-1)/n;
                     int c = (i-1)%n;
                     
-                    if(r%2==n%2)
+                    if((n-r-1)%2)
                         c=n-1-c;
-                    //cout<<r<<" "<<c<<endl;
+                   
                     if(board[r][c]==-1 and visited[i]==0)
                     {
                         visited[i]=1;
@@ -37,7 +37,6 @@ public:
                     
                     else if(board[r][c]!=-1 and visited[board[r][c]]==0)
                     {
-                      
                         visited[board[r][c]] = 1;
                         q.push(board[r][c]);
                     }  
@@ -47,57 +46,59 @@ public:
         }
         return -1;
     }
-    
-    /*
-    vector<int> findcordinates(int pos,int n){
-          vector<int>temp;
-            int r=n-(pos-1)/n-1;
-            int c=(pos-1)%n;
-            if(r%2==n%2){
-                return temp={r,n-1-c};
-            }else{
-                return temp={r,c};
-            }
-            }
-
-
-    int snakesAndLadders(vector<vector<int>>& board) {
-        int n= board.size();
-        queue<int>q;
-        vector<vector<bool>>visited( n , vector<bool>(n, false)); 
-        int ans=0;
-        q.push(1);
-        visited[n-1][0]=true;
-        while(!q.empty()){
-            int size=q.size();
-           
-            for(int i=0;i<size;++i){
-                int front=q.front();
-                q.pop();
-                if(front==n*n) return ans;
-                for(int k=1;k<=6;++k){
-                    if(k+front>n*n) break;
-                    vector<int>pos=findcordinates(front+k,n);
-                    int r=pos[0];
-                    int c=pos[1];
-                    if(visited[r][c]==true) 
-                        continue;
-                    visited[r][c]=true;
-                    if(board[r][c]!=-1)
-                    {
-                        q.push(board[r][c]);
-                    }
-                    else
-                    {
-                        q.push(front+k);
-                    }
-
-                }
-            }
-            ans++;
-        }
-        return -1;
-        }
-        */
+   
     
 };
+
+ 
+/*
+vector<int> findcordinates(int pos,int n){
+  vector<int>temp;
+    int r=n-(pos-1)/n-1;
+    int c=(pos-1)%n;
+    if(r%2==n%2){
+        return temp={r,n-1-c};
+    }else{
+        return temp={r,c};
+    }
+    }
+
+
+int snakesAndLadders(vector<vector<int>>& board) {
+int n= board.size();
+queue<int>q;
+vector<vector<bool>>visited( n , vector<bool>(n, false)); 
+int ans=0;
+q.push(1);
+visited[n-1][0]=true;
+while(!q.empty()){
+    int size=q.size();
+
+    for(int i=0;i<size;++i){
+        int front=q.front();
+        q.pop();
+        if(front==n*n) return ans;
+        for(int k=1;k<=6;++k){
+            if(k+front>n*n) break;
+            vector<int>pos=findcordinates(front+k,n);
+            int r=pos[0];
+            int c=pos[1];
+            if(visited[r][c]==true) 
+                continue;
+            visited[r][c]=true;
+            if(board[r][c]!=-1)
+            {
+                q.push(board[r][c]);
+            }
+            else
+            {
+                q.push(front+k);
+            }
+
+        }
+    }
+    ans++;
+}
+return -1;
+}
+*/
