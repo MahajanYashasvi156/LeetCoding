311726d3fa4420321fda3d1f46ed405310ffd6d8
MahajanYashasvi156 <95318914+MahajanYashasvi156@users.noreply.github.com>
2023-01-26 23:43:08 +0530

    Time: 54 ms (30.01%), Space: 14.2 MB (26.34%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,116 @@
+/*
+Dijisktra Algorithm - 
+
+Here there are two factors to decide the shortest path 
+1. Cost should be minimum
+2. If at any node we are reaching with lesser level then the chances of reaching to the destination increases because it may possible that cost to reach at current cell is high but it may be lead to us ans. 
+
+*/
+
+typedef pair<int,pair<int,int>> p;//cost, dest,level
+class Solution 
+{
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k)
+    {
+        vector<vector<pair<int,int>>> adj(n);
+        priority_queue<p,vector<p>,greater<p>> minHeap;
+        
+        vector<int> stops(n,INT_MAX);
+        for(auto a: flights)
+        {
+            int start = a[0];
+            int dest = a[1];
+            int price = a[2];
+            adj[start].push_back({dest,price});
+        }
+        
+        minHeap.push({0,{src,0}});
+      
+        while(!minHeap.empty())
+        {
+            int weight = minHeap.top().first;
+            int start = minHeap.top().second.first;
+            int level = minHeap.top().second.second;
+            minHeap.pop();
+             
+            
+            if(start==dst)
+                return weight;
+            if(level>k)
+                continue;
+            if(stops[start]<level)
+                continue;
+            stops[start]= level;
+                
+            for(auto a:adj[start])
+            {
+                int dest = a.first;
+                int price = a.second;
+                minHeap.push({weight+price,{dest,level+1}}); 
+            }
+        }
+        return -1;
+    }
+};
+
+/*
+
+Dijisktra Algorithm - 
+
+Here there are two factors to decide the shortest path 
+1. Cost should be minimum
+2. If at any node we are reaching with lesser level then the chances of reaching to the destination increases because it may possible that cost to reach at current cell is high but it may be lead to us ans. 
+
+
+typedef pair<int,pair<int,int>> p;//cost, dest,level
+class Solution 
+{
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k)
+    {
+        vector<vector<pair<int,int>>> adj(n);
+        priority_queue<p,vector<p>,greater<p>> minHeap;
+        //distance + level
+        vector<pair<int,int>> distance(n,{INT_MAX,INT_MAX});
+        for(auto a: flights)
+        {
+            int start = a[0];
+            int dest = a[1];
+            int price = a[2];
+            adj[start].push_back({dest,price});
+        }
+        
+        minHeap.push({0,{src,0}});
+        distance[src]={0,0};
+        
+        while(!minHeap.empty())
+        {
+            int weight = minHeap.top().first;
+            int start = minHeap.top().second.first;
+            int level = minHeap.top().second.second;
+            minHeap.pop();
+                  
+            if(start==dst)
+                return weight;
+            for(auto a:adj[start])
+            {
+                int dest = a.first;
+                int price = a.second;
+                
+                //If we have atleast one more stop left then we can directly move to next stop without knowing whther it is destination or not. But if we have no more stops left the next move must be at destination only.
+                if(level==k and dest==dst or level<k)
+                {
+                    if(distance[dest].first>weight+price or distance[dest].second>level+1)
+                    {
+                        distance[dest].first=weight+price;
+                        distance[dest].second = level+1;
+                        minHeap.push({distance[dest].first,{dest,level+1}});
+                    }     
+                }
+            }
+        }
+        return -1;
+    }
+};
+*/
